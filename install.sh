15c22252628ef282f5db9253d36530e097dec361
Chris Cooper <chris.cooper@nicta.com.au>
2015-07-07 15:56:55 +1000

    use git submodule for ceres
    Former-commit-id: ef5649e4ec23c8ed43c70c4dbad4c5a3cc12693f

--- install.sh
+++ install.sh
@@ -173,7 +173,6 @@ vlfeat.tar.gz http://www.vlfeat.org/download/vlfeat-0.9.13-bin.tar.gz
 cmvs.tar.gz http://www.di.ens.fr/cmvs/cmvs-fix2.tar.gz
 graclus.tar.gz http://smathermather.github.io/BundlerTools/patched_files/src/graclus/graclus1.2.tar.gz
 pcl.tar.gz https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.2.tar.gz
-ceres-solver.tar.gz http://ceres-solver.org/ceres-solver-1.10.0.tar.gz
 EOF
 
 echo "  < done - `date`"
@@ -201,7 +200,6 @@ mv -f parallel-20141022   "$PARALLEL_PATH"
 mv -f PoissonRecon        "$PSR_PATH"
 mv -f cmvs                "$CMVS_PATH"
 mv -f pcl-pcl-1.7.2       "$PCL_PATH"
-mv -f ceres-solver-1.10.0 "$CERES_PATH"
 
 echo "  < done - `date`"
 
