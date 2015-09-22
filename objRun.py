b6514587e5a4373bde9c8526c4c6be1f47cc3cbf
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2015-09-22 12:23:00 -0700

    clean commit finally

--- objRun.py
+++ objRun.py
@@ -127,15 +127,19 @@ class ODMJob:
                                          + objPhotos.dictStepVals["step_0_resizedImage"])
                         
                         run("convert -resize " + str(self.resizeTo) + "x" + str(self.resizeTo)\
-                            +" -quality 100 \"" + self.srcDir + "/" + objPhotos.strFileName + "\" \"" + objPhotos.dictStepVals["step_0_resizedImage"] + "\"")
+                            +" -quality 100 \"" + self.srcDir + "/" + objPhotos.strFileName + "\" \"" \
+                            + objPhotos.dictStepVals["step_0_resizedImage"] + "\"")
                     
                     else:
-                        sys.stdout.write("     copying " + objPhotos.strFileName + " \tto " + objPhotos.dictStepVals["step_0_resizedImage"])
+                        sys.stdout.write("     copying " + objPhotos.strFileName + " \tto " \
+                                         + objPhotos.dictStepVals["step_0_resizedImage"])
                         shutil.copyfile(CURRENT_DIR + "/" + objPhotos.strFileName, objPhotos.dictStepVals["step_0_resizedImage"])
                 else:	
-                    print "     using existing " + objPhotos.strFileName + " \tto " + objPhotos.dictStepVals["step_0_resizedImage"]
+                    print "     using existing " + objPhotos.strFileName + " \tto " \
+                          + objPhotos.dictStepVals["step_0_resizedImage"]
     
-                file_resolution = runAndReturn('jhead "' + objPhotos.dictStepVals["step_0_resizedImage"] + '"', 'grep "Resolution"')
+                file_resolution = runAndReturn('jhead "' + objPhotos.dictStepVals["step_0_resizedImage"] \
+                                               + '"', 'grep "Resolution"')
                 match = re.search(": ([0-9]*) x ([0-9]*)", file_resolution)
                 if match:
                     objPhotos.width = int(match.group(1).strip())
@@ -192,7 +196,8 @@ class ODMJob:
         siftDest.write(vlsiftJobs)
         siftDest.close()
     
-        run("\"" + BIN_PATH + "/parallel\" --no-notice --halt-on-error 1 -j+0 < \"" + self.dictJobLocations["step_1_vlsift"] + "\"")
+        run("\"" + BIN_PATH + "/parallel\" --no-notice --halt-on-error 1 -j+0 < \"" \
+            + self.dictJobLocations["step_1_vlsift"] + "\"")
         
     def match():
         print "\n  - matching keypoints - " + now()
@@ -252,7 +257,11 @@ class ODMJob:
                 for j in range(i + 1, objectStats["good"]):
                     c += 1
                     if not os.path.isfile(jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt"):
-                        matchesJobs += "echo -n \".\" && touch \"" + jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt\" && \"" + BIN_PATH + "/KeyMatch\" \"" + objects[i]["step_1_keyFile"] + "\" \"" + objects[j]["step_1_keyFile"] + "\" \"" + jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt\" " + str(args['--matcher-ratio']) + " " + str(args['--matcher-threshold']) + "\n"
+                        matchesJobs += "echo -n \".\" && touch \"" + jobOptions["step_2_matches_dir"] + "/" + str(i)     \
+                            + "-" + str(j) + ".txt\" && \"" + BIN_PATH + "/KeyMatch\" \"" + objects[i]["step_1_keyFile"] \
+                            + "\" \"" + objects[j]["step_1_keyFile"] + "\" \"" + jobOptions["step_2_matches_dir"] + "/"  \
+                            + str(i) + "-" + str(j) + ".txt\" " + str(args['--matcher-ratio']) + " "                     \
+                            + str(args['--matcher-threshold']) + "\n"
     
         matchDest = open(jobOptions["step_2_macthes_jobs"], 'w')
         matchDest.write(matchesJobs)
@@ -264,8 +273,11 @@ class ODMJob:
         for i in range(0, objectStats["good"]):
             for j in range(i + 1, objectStats["good"]):            
                 c += 1            
-                if os.path.isfile(jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt") and os.path.getsize(jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt") > 0:                
-                    run("echo \"" + str(i) + " " + str(j) + "\" >> \"" + jobOptions["step_2_matches"] + "\" && cat \"" + jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt\" >> \"" + jobOptions["step_2_matches"] + "\"")
+                if os.path.isfile(jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt") and          \
+                   os.path.getsize(jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j) + ".txt") > 0:                
+                    run("echo \"" + str(i) + " " + str(j) + "\" >> \"" + jobOptions["step_2_matches"]                   \
+                        + "\" && cat \"" + jobOptions["step_2_matches_dir"] + "/" + str(i) + "-" + str(j)               \
+                        +  ".txt\" >> \"" + jobOptions["step_2_matches"] + "\"")
     
         filesList = ""
         for fileObject in objects:
@@ -276,7 +288,7 @@ class ODMJob:
         matchDest.write(filesList)
         matchDest.close()
     
-        #   run("\"" + BIN_PATH + "/KeyMatchFull\" \"" + jobOptions["step_2_filelist"] + "\" \"" + jobOptions["step_2_matches"] + "\"    ")
+
         
 class ODMPhoto:
     """   ODMPhoto - a class for ODMPhotos
@@ -760,7 +772,8 @@ def parse_coordinate_system():
                     if len(tokens) == 3:
                         utmZoneString = tokens[2][0:len(tokens[2])-2].strip()
                         utmSouthBool = (tokens[2][len(tokens[2])-2].strip() == 'S')
-                        jobOptions['csString'] = '+datum=WGS84 +proj=utm +zone=' + utmZoneString + (' +south' if utmSouthBool else '')
+                        jobOptions['csString'] = '+datum=WGS84 +proj=utm +zone='               \
+                            + utmZoneString + (' +south' if utmSouthBool else '')
                         jobOptions['epsg'] = calculate_EPSG(int(utmZoneString), utmSouthBool)
                 elif lineNumber == 1:
                     tokens = line.split(' ')
@@ -886,7 +899,8 @@ def bundler():
     bundlerDest.write(filesList)
     bundlerDest.close()
 
-    run("\"" + BIN_PATH + "/bundler\" \"" + jobOptions["step_3_filelist"] + "\" --options_file \"" + jobOptions["step_3_bundlerOptions"] + "\" > bundle/out")
+    run("\"" + BIN_PATH + "/bundler\" \"" + jobOptions["step_3_filelist"] + "\" --options_file \"" \
+        + jobOptions["step_3_bundlerOptions"] + "\" > bundle/out")
     run("\"" + BIN_PATH + "/Bundle2PMVS\" \"" + jobOptions["step_3_filelist"] + "\" bundle/bundle.out")
     run("\"" + BIN_PATH + "/RadialUndistort\" \"" + jobOptions["step_3_filelist"] + "\" bundle/bundle.out pmvs")
 
@@ -1002,7 +1016,13 @@ def odm_texturing():
         pass
 
  
-    run("\"" + BIN_PATH + "/odm_texturing\" -bundleFile " + jobOptions["jobDir"] + "/pmvs/bundle.rd.out -imagesPath "+ jobOptions["srcDir"] + "/ -imagesListPath " + jobOptions["jobDir"] + "/pmvs/list.rd.txt -inputModelPath " + jobOptions["jobDir"] + "-results/odm_mesh-0000.ply -outputFolder " + jobOptions["jobDir"] + "-results/odm_texturing/ -textureResolution " + str(args['--odm_texturing-textureResolution']) + " -bundleResizedTo " + str(jobOptions["resizeTo"]) + " -textureWithSize " + str(args['--odm_texturing-textureWithSize']) + " -logFile " + jobOptions["jobDir"] + "/odm_texturing/odm_texturing_log.txt")
+    run("\"" + BIN_PATH + "/odm_texturing\" -bundleFile " + jobOptions["jobDir"] + "/pmvs/bundle.rd.out -imagesPath "\
+        + jobOptions["srcDir"] + "/ -imagesListPath " + jobOptions["jobDir"] + "/pmvs/list.rd.txt -inputModelPath "  \
+        + jobOptions["jobDir"] + "-results/odm_mesh-0000.ply -outputFolder " + jobOptions["jobDir"]                  \
+        + "-results/odm_texturing/ -textureResolution " + str(args['--odm_texturing-textureResolution'])             \
+        + " -bundleResizedTo " + str(jobOptions["resizeTo"]) + " -textureWithSize " +                                \
+        str(args['--odm_texturing-textureWithSize']) + " -logFile " + jobOptions["jobDir"]                           \
+        + "/odm_texturing/odm_texturing_log.txt")
 
     if args['--end-with'] != "odm_texturing":
         odm_georeferencing()
@@ -1019,10 +1039,32 @@ def odm_georeferencing():
         pass
 
     if not args.odm_georeferencing_useGcp:
-        run("\"" + BIN_PATH + "/odm_extract_utm\" -imagesPath " + jobOptions["srcDir"] + "/ -imageListFile " + jobOptions["jobDir"] + "/pmvs/list.rd.txt -outputCoordFile " + jobOptions["jobDir"] + "/odm_georeferencing/coordFile.txt")
-        run("\"" + BIN_PATH + "/odm_georef\" -bundleFile " + jobOptions["jobDir"] + "/pmvs/bundle.rd.out -inputCoordFile " + jobOptions["jobDir"] + "/odm_georeferencing/coordFile.txt -inputFile " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model.obj -outputFile " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model_geo.obj -inputPointCloudFile " + jobOptions["jobDir"] + "-results/option-0000.ply -outputPointCloudFile " + jobOptions["jobDir"] + "-results/option-0000_georef.ply -logFile " + jobOptions["jobDir"] + "/odm_georeferencing/odm_georeferencing_log.txt -georefFileOutputPath " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model_geo_georef_system.txt")
+        run("\"" + BIN_PATH + "/odm_extract_utm\" -imagesPath " + jobOptions["srcDir"] + "/ -imageListFile " \
+            + jobOptions["jobDir"] + "/pmvs/list.rd.txt -outputCoordFile " + jobOptions["jobDir"]            \
+            + "/odm_georeferencing/coordFile.txt")
+        
+        run("\"" + BIN_PATH + "/odm_georef\" -bundleFile " + jobOptions["jobDir"]                            \
+            + "/pmvs/bundle.rd.out -inputCoordFile " + jobOptions["jobDir"]                                  \
+            + "/odm_georeferencing/coordFile.txt -inputFile " + jobOptions["jobDir"]                         \
+            + "-results/odm_texturing/odm_textured_model.obj -outputFile " + jobOptions["jobDir"]            \
+            + "-results/odm_texturing/odm_textured_model_geo.obj -inputPointCloudFile "                      \
+            + jobOptions["jobDir"] + "-results/option-0000.ply -outputPointCloudFile " + jobOptions["jobDir"] \
+            + "-results/option-0000_georef.ply -logFile " + jobOptions["jobDir"]                             \
+            + "/odm_georeferencing/odm_georeferencing_log.txt -georefFileOutputPath " + jobOptions["jobDir"] \
+            + "-results/odm_texturing/odm_textured_model_geo_georef_system.txt")
+        
     elif os.path.isfile(jobOptions["srcDir"] + "/" + args.odm_georeferencing_gcpFile):
-        run("\"" + BIN_PATH + "/odm_georef\" -bundleFile " + jobOptions["jobDir"] + "/pmvs/bundle.rd.out -gcpFile " + jobOptions["srcDir"] + "/" + args.odm_georeferencing_gcpFile + " -imagesPath " + jobOptions["srcDir"] + "/ -imagesListPath " + jobOptions["jobDir"] + "/pmvs/list.rd.txt -bundleResizedTo " + str(jobOptions["resizeTo"]) + " -inputFile " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model.obj -outputFile " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model_geo.obj -outputCoordFile " + jobOptions["jobDir"] + "/odm_georeferencing/coordFile.txt -inputPointCloudFile " + jobOptions["jobDir"] + "-results/option-0000.ply -outputPointCloudFile " + jobOptions["jobDir"] + "-results/option-0000_georef.ply -logFile " + jobOptions["jobDir"] + "/odm_georeferencing/odm_georeferencing_log.txt -georefFileOutputPath " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model_geo_georef_system.txt")
+        run("\"" + BIN_PATH + "/odm_georef\" -bundleFile " + jobOptions["jobDir"]                             \
+            + "/pmvs/bundle.rd.out -gcpFile " + jobOptions["srcDir"] + "/" + args.odm_georeferencing_gcpFile  \
+            + " -imagesPath " + jobOptions["srcDir"] + "/ -imagesListPath " + jobOptions["jobDir"]            \
+            + "/pmvs/list.rd.txt -bundleResizedTo " + str(jobOptions["resizeTo"]) + " -inputFile "            \
+            + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model.obj -outputFile "             \
+            + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model_geo.obj -outputCoordFile "    \
+            + jobOptions["jobDir"] + "/odm_georeferencing/coordFile.txt -inputPointCloudFile "                \
+            + jobOptions["jobDir"] + "-results/option-0000.ply -outputPointCloudFile " + jobOptions["jobDir"] \
+            + "-results/option-0000_georef.ply -logFile " + jobOptions["jobDir"]                              \
+            + "/odm_georeferencing/odm_georeferencing_log.txt -georefFileOutputPath " + jobOptions["jobDir"]  \
+            + "-results/odm_texturing/odm_textured_model_geo_georef_system.txt")
     else:
         print "Warning: No GCP file. Consider rerunning with argument --odm_georeferencing-useGcp false --start-with odm_georeferencing"
         print "Skipping orthophoto"
@@ -1041,12 +1083,18 @@ def odm_georeferencing():
                 for lineNumber, line in enumerate(f):
                     if lineNumber >= 2 and lineNumber - 2 < len(images):
                         tokens = line.split(' ')
+                        
                         if len(tokens) >= 3:
                             x = float(tokens[0])
                             y = float(tokens[1])
                             z = float(tokens[2])
                             filename = images[lineNumber - 2]
-                            run("echo " + str(x + jobOptions["utmEastOffset"]) + " " + str(y + jobOptions["utmNorthOffset"]) + " " + str(z) + " | cs2cs " + jobOptions["csString"] + " +to +datum=WGS84 +proj=latlong > " + jobOptions["jobDir"] + "/odm_georeferencing/latlong.txt")
+                            
+                            run("echo " + str(x + jobOptions["utmEastOffset"]) + " "                          \
+                                + str(y + jobOptions["utmNorthOffset"]) + " " + str(z)                        \
+                                + " | cs2cs " + jobOptions["csString"] + " +to +datum=WGS84 +proj=latlong > " \
+                                + jobOptions["jobDir"] + "/odm_georeferencing/latlong.txt")
+                            
                             with open(jobOptions["jobDir"] + "/odm_georeferencing/latlong.txt") as latlongFile:
                                 latlongLine = latlongFile.readline()
                                 tokens = latlongLine.split()
@@ -1081,12 +1129,18 @@ def odm_georeferencing():
                                             latRef = tokens[3]
 
                                             exivCmd = "exiv2 -q"
-                                            exivCmd += " -M\"set Exif.GPSInfo.GPSLatitude " + latDeg + "/1 " + latMin + "/1 " + latSecNumerator + "/" + latSecDenominator + "\""
+                                            exivCmd += " -M\"set Exif.GPSInfo.GPSLatitude " + latDeg + "/1 "         \
+                                                + latMin + "/1 " + latSecNumerator + "/" + latSecDenominator + "\""
+                                            
                                             exivCmd += " -M\"set Exif.GPSInfo.GPSLatitudeRef " + latRef + "\""
-                                            exivCmd += " -M\"set Exif.GPSInfo.GPSLongitude " + lonDeg + "/1 " + lonMin + "/1 " + lonSecNumerator + "/" + lonSecDenominator + "\""
+                                            
+                                            exivCmd += " -M\"set Exif.GPSInfo.GPSLongitude " + lonDeg + "/1 "         \
+                                                + lonMin + "/1 " + lonSecNumerator + "/" + lonSecDenominator + "\""
+                                            
                                             exivCmd += " -M\"set Exif.GPSInfo.GPSLongitudeRef " + lonRef + "\""
 
                                             altNumerator = arcDenominator = 0  # BUG: arcDenominator is never used
+                                            
                                             if altString:
                                                 altFrac = fractions.Fraction(altString)
                                                 altNumerator = str(altFrac._numerator)
@@ -1099,11 +1153,19 @@ def odm_georeferencing():
                                             exifGpsInfoWritten = True
 
                                     if not exifGpsInfoWritten:
-                                        print("    Warning: Failed setting EXIF GPS info for " + filename + " based on " + latlongLine)
+                                        print("    Warning: Failed setting EXIF GPS info for " \
+                                              + filename + " based on " + latlongLine)
 
     if "epsg" in jobOptions and "utmEastOffset" in jobOptions and "utmNorthOffset" in jobOptions:
-        lasCmd = "\"" + BIN_PATH + "/txt2las\" -i " + jobOptions["jobDir"] + "-results/option-0000_georef.ply -o " + jobOptions["jobDir"] + "-results/pointcloud_georef.laz -skip 30 -parse xyzRGBssss -set_scale 0.01 0.01 0.01 -set_offset " + str(jobOptions["utmEastOffset"]) + " " + str(jobOptions["utmNorthOffset"]) + " 0 -translate_xyz " + str(jobOptions["utmEastOffset"]) + " " + str(jobOptions["utmNorthOffset"]) + " 0 -epsg " + str(jobOptions["epsg"])
+        lasCmd = "\"" + BIN_PATH + "/txt2las\" -i " + jobOptions["jobDir"] +                                         \
+            "-results/option-0000_georef.ply -o " + jobOptions["jobDir"]                                             \
+            + "-results/pointcloud_georef.laz -skip 30 -parse xyzRGBssss -set_scale 0.01 0.01 0.01 -set_offset "     \
+            + str(jobOptions["utmEastOffset"]) + " " + str(jobOptions["utmNorthOffset"]) + " 0 -translate_xyz "      \
+            + str(jobOptions["utmEastOffset"]) + " " + str(jobOptions["utmNorthOffset"])                             \
+            + " 0 -epsg " + str(jobOptions["epsg"])
+        
         print("    Creating geo-referenced LAS file (expecting warning)...")
+        
         run(lasCmd)
  
 
@@ -1121,7 +1183,11 @@ def odm_orthophoto():
     except:
         pass
 
-    run("\"" + BIN_PATH + "/odm_orthophoto\" -inputFile " + jobOptions["jobDir"] + "-results/odm_texturing/odm_textured_model_geo.obj -logFile " + jobOptions["jobDir"] + "/odm_orthophoto/odm_orthophoto_log.txt -outputFile " + jobOptions["jobDir"] + "-results/odm_orthphoto.png -resolution 20.0 -outputCornerFile " + jobOptions["jobDir"] + "/odm_orthphoto_corners.txt")
+    run("\"" + BIN_PATH + "/odm_orthophoto\" -inputFile " + jobOptions["jobDir"] +                \
+        "-results/odm_texturing/odm_textured_model_geo.obj -logFile " + jobOptions["jobDir"]      \
+        + "/odm_orthophoto/odm_orthophoto_log.txt -outputFile " + jobOptions["jobDir"]            \
+        + "-results/odm_orthphoto.png -resolution 20.0 -outputCornerFile " + jobOptions["jobDir"] \
+        + "/odm_orthphoto_corners.txt")
 
     if "csString" not in jobOptions:
         parse_coordinate_system()
@@ -1172,6 +1238,10 @@ if __name__ == '__main__':
     intStart = -1
     intEnd = -2
     
+    #  Create a dict for holding our steps
+    #       key is step number, val is call
+    #       Construct the calls below in eval by iterating through the composed dict of steps
+    #       -->  Allows for running steps in any arbitray sequence, e.g. rebatching certain steps
     for keys in dictSteps.keys():
         if dictSteps[keys] == objODMJob.args.start_with:
             intStart = keys
@@ -1187,6 +1257,7 @@ if __name__ == '__main__':
     for steps in listJobQueue:
         methodCall = steps+"()"
         strEval = "objODMJob."+methodCall
+        #  EVAL safety - *only* internally generated strings (no user strings)
         eval(strEval)
 
     if args.zip_results:
