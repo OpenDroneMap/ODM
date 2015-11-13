28128710fc8168c62c0d6643c17165031d5c23c5
edgarriba <edgar.riba@gmail.com>
2015-11-13 15:13:09 +0000

    add external openvg
    Former-commit-id: e40516b5cbf2ff08e979cea11c7d9bb6f93c4e06

--- SuperBuild/cmake/External-OpenVG.cmake
+++ SuperBuild/cmake/External-OpenVG.cmake
@@ -1,26 +1,26 @@
-set(_proj_name opensfm)
+set(_proj_name openvg)
 set(_SB_BINARY_DIR "${CMAKE_BINARY_DIR}/${_proj_name}")
 
 ExternalProject_Add(${_proj_name}
-  DEPENDS           ceres opencv openvg
   PREFIX            ${_SB_BINARY_DIR}
   TMP_DIR           ${_SB_BINARY_DIR}/tmp
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  URL               https://github.com/mapillary/OpenSfM/archive/odm-1.zip
-  URL_MD5           5261d2df9af2b29a3bfde0b29421d108
+  URL               https://github.com/paulinus/opengv/archive/python-wrapper.zip
+  URL_MD5           6afd5dfbec8f11b556e794d009bbbcc2
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
   SOURCE_DIR        ${SB_SOURCE_DIR}/${_proj_name}
-  CONFIGURE_COMMAND cmake <SOURCE_DIR>/${_proj_name}/src
-    -DCERES_ROOT_DIR=${SB_INSTALL_DIR} 
-    -DOpenCV_DIR=${SB_INSTALL_DIR}/share/OpenCV
+  CMAKE_ARGS
+    -DBUILD_TESTS=OFF 
+    -DBUILD_PYTHON=ON
+    -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
   #--Build step-----------------
   BINARY_DIR        ${_SB_BINARY_DIR}
   #--Install step---------------
-  INSTALL_COMMAND    ""
+  INSTALL_DIR       ${SB_INSTALL_DIR}
   #--Output logging-------------
   LOG_DOWNLOAD      OFF
   LOG_CONFIGURE     OFF
