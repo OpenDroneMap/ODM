5f52ea1efce12851be90807794d40726bac16908
edgarriba <edgar.riba@gmail.com>
2015-11-16 11:09:29 +0000

    add cmvs internal installation

---
+++ SuperBuild/cmake/External-CMVS.cmake
@@ -0,0 +1,27 @@
+set(_proj_name cmvs)
+set(_SB_BINARY_DIR "${CMAKE_BINARY_DIR}/${_proj_name}")
+
+ExternalProject_Add(${_proj_name}
+  PREFIX            ${_SB_BINARY_DIR}
+  TMP_DIR           ${_SB_BINARY_DIR}/tmp
+  STAMP_DIR         ${_SB_BINARY_DIR}/stamp
+  #--Download step--------------
+  DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
+  URL               https://github.com/edgarriba/CMVS-PMVS/archive/master.zip
+  URL_MD5           dbb1493f49ca099b4208381bd20d1435
+  #--Update/Patch step----------
+  UPDATE_COMMAND    ""
+  #--Configure step-------------
+  SOURCE_DIR        ${SB_SOURCE_DIR}/${_proj_name}
+  CONFIGURE_COMMAND cmake <SOURCE_DIR>/program
+    -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
+  #--Build step-----------------
+  BINARY_DIR        ${_SB_BINARY_DIR}
+  #--Install step---------------
+  INSTALL_DIR       ${SB_INSTALL_DIR}
+  #--Output logging-------------
+  LOG_DOWNLOAD      OFF
+  LOG_CONFIGURE     OFF
+  LOG_BUILD         OFF
+)
+
