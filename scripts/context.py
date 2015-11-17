5b36bfed642512712512c3ee5558e9a46f1a1871
edgarriba <edgar.riba@gmail.com>
2015-11-17 12:38:56 +0000

    add context
    Former-commit-id: 8c102027c17b5f4cfbc6cbe5109aad811d0c4ab6

---
+++ scripts/context.py
@@ -0,0 +1,8 @@
+import os
+
+# Define third party libs location
+scripts_path = os.path.abspath(os.path.dirname(__file__))
+superbuild_path = os.path.join(scripts_path[:-7], 'SuperBuild/')
+
+pyopencv_path = os.path.join(superbuild_path, 'lib/python2.7/dist-packages')
+opensfm_path = os.path.join(superbuild_path, "src/opensfm")
