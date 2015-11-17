fcd490ac35c37bd979d5a8ff7895c3f665a3d3db
edgarriba <edgar.riba@gmail.com>
2015-11-17 13:59:19 +0000

    add usage application log
    Former-commit-id: 03b4a341b89d35d840b3d95066a8221a1e0059cd

--- scripts/run.py
+++ scripts/run.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
-import context
+import sys
+
 import system
 import log
 
 from datatypes import ODMApp
 
-	
+def usage():
+	log.ODM_WARNING('USAGE: %s dataset_path' % sys.argv[0])
+ 	log.ODM_ERROR('OpenDroneMap app finished - %s' % system.now())
+ 	sys.exit(0)
+		
 if __name__ == '__main__':
 
 	log.ODM_INFO('Initializing OpenDroneMap app - %s' % system.now())
 
+	if len(sys.argv) < 2:
+		usage()
+	else:
+		dataset_path = sys.argv[1]
+
 	# Initialize odm app
 	# internally configure all tasks
-	app = ODMApp(context.scripts_path)
+	app = ODMApp(dataset_path)
 
 	# set from where we want to start
 	# by default we will start from the beginnig
