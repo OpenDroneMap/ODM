a1dd4448bfa96da9990e7de1f13f190ee138ac89
edgarriba <edgar.riba@gmail.com>
2015-11-18 16:40:10 +0000

    refactor main method
    Former-commit-id: edc6c94e6ab09c73fabc15d2d4a578ef672e5e5b

--- scripts/run.py
+++ scripts/run.py
@@ -2,13 +2,14 @@
 
 import sys
 
-import system
 import log
+import config
+import system
 
 from datatypes import ODMApp
 
 def usage():
-	log.ODM_WARNING('USAGE: %s dataset_path' % sys.argv[0])
+	log.ODM_ERROR('USAGE: %s --images-src dataset_path' % sys.argv[0])
  	log.ODM_ERROR('OpenDroneMap app finished - %s' % system.now())
  	sys.exit(0)
 		
@@ -16,22 +17,21 @@ if __name__ == '__main__':
 
 	log.ODM_INFO('Initializing OpenDroneMap app - %s' % system.now())
 
-	if len(sys.argv) < 2:
+	# Force to provide the images path
+	if config.args.get('images_src') is None:
 		usage()
-	else:
-		dataset_path = sys.argv[1]
 
 	# Initialize odm app
 	# internally configure all tasks
-	app = ODMApp(dataset_path)
+	app = ODMApp(config.args)
 
 	# set from where we want to start
 	# by default we will start from the beginnig
 	init_task_id = 0
 
-	log.ODM_INFO('Runnning OpenDroneMap app from state %s - %s' % (init_task_id, system.now()))
+	log.ODM_INFO('Runnning OpenDroneMap app - %s' % system.now())
 
 	# run all tasks
-	app.run(init_task_id)
+	app.run_all(init_task_id)
 
  	log.ODM_INFO('OpenDroneMap app finished - %s' % system.now())
