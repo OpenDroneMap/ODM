9986cdbd1d47ab3f545e391ea7054666c7e944a5
edgarriba <edgar.riba@gmail.com>
2015-11-18 16:37:41 +0000

    add ccd widths path
    Former-commit-id: 490b0827baa94a79a2a32ca755af5c85b526bade

--- scripts/context.py
+++ scripts/context.py
@@ -1,11 +1,15 @@
 import os
 
-# Define third party libs location
+# Define some needed locations
 scripts_path = os.path.abspath(os.path.dirname(__file__))
-superbuild_path = os.path.join(scripts_path[:-7], 'SuperBuild/')
+root_path, _ = os.path.split(scripts_path)
+
+superbuild_path = os.path.join(root_path, 'SuperBuild')
+ccd_widths_path = os.path.join(root_path, 'ccd_defs.json')
 
 pyopencv_path = os.path.join(superbuild_path, 'lib/python2.7/dist-packages')
 opensfm_path = os.path.join(superbuild_path, "src/opensfm")
+
 
 # Define supported image extensions
 supported_extensions = {'.jpg','.jpeg'}
