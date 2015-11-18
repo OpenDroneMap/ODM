237ec8a5d3dafe0a83912e33b238415570134bc3
edgarriba <edgar.riba@gmail.com>
2015-11-18 16:38:29 +0000

    add path extaction tools
    Former-commit-id: b068d439bb1bc97d25edc27b5349199b01458fb4

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,5 +1,6 @@
 import os
 
+import log
 import context
 import datatypes
 
@@ -17,11 +18,21 @@ def load_dataset(images_dir, args, photos):
     # by now only 'jpg' and 'jpeg are supported
     files = [f for f in files if supported_extension(f)]
 
-    #photos = []
+    if len(files) < 2:
+        log.ODM_ERROR('Not found any supported image in %s' % images_dir)
+        return False
 
     # create ODMPhoto list
     for f in files:
         file_name = os.path.join(images_dir, f)
         photos.append(datatypes.ODMPhoto(file_name, args))
+    return True
 
-    #return photos
+def extract_file_from_path_file(path_file):
+    path, file = os.path.split(path_file)
+    return file
+
+def extract_path_from_file(file):
+    path_file = os.path.abspath(os.path.dirname(file))
+    path, file = os.path.split(path_file)
+    return path
