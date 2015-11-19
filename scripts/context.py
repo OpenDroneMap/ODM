c5ed1e5fae67ab5bff79649d0e9000476f6753e8
edgarriba <edgar.riba@gmail.com>
2015-11-19 12:00:34 +0000

    configure pyopencv

--- scripts/context.py
+++ scripts/context.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 # Define some needed locations
 scripts_path = os.path.abspath(os.path.dirname(__file__))
@@ -7,9 +8,11 @@ root_path, _ = os.path.split(scripts_path)
 superbuild_path = os.path.join(root_path, 'SuperBuild')
 ccd_widths_path = os.path.join(root_path, 'ccd_defs.json')
 
-pyopencv_path = os.path.join(superbuild_path, 'lib/python2.7/dist-packages')
-opensfm_path = os.path.join(superbuild_path, "src/opensfm")
+# add opencv to python path
+pyopencv_path = os.path.join(superbuild_path, 'install/lib/python2.7/dist-packages')
+sys.path.append(pyopencv_path)
 
+opensfm_path = os.path.join(superbuild_path, "src/opensfm")
 
 # Define supported image extensions
 supported_extensions = {'.jpg','.jpeg'}
