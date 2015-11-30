099b3497d0b32db6aa1502fa77917c0907a8e779
Stephen Mather <stephen@smathermather.com>
2015-11-29 21:52:32 -0500

    Fix to gcp reading order
    Former-commit-id: 1128c40f9e661f2a4523eed9960f56c3dd6cd86b

--- odm_georef/src/Georef.cpp
+++ odm_georef/src/Georef.cpp
@@ -28,7 +28,7 @@ GeorefGCP::~GeorefGCP()
 
 void GeorefGCP::extractGCP(std::istringstream &gcpStream)
 {
-    gcpStream >> x_ >> y_ >> z_ >> pixelY_ >> pixelX_ >> image_;
+    gcpStream >> x_ >> y_ >> z_ >> pixelX_ >> pixelY_ >> image_;
 }
 
 Vec3 GeorefGCP::getPos()
