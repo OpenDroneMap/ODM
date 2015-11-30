be86eba20a4c8c4c348683f6599bdf5f5635ac4c
edgarriba <edgar.riba@gmail.com>
2015-11-30 15:49:52 +0000

    add path to modules binaries
    Former-commit-id: 68636e4ba5025dc397bf9051ec10bdae7880abf9

--- opendm/context.py
+++ opendm/context.py
@@ -13,11 +13,14 @@ ccd_widths_path = os.path.join(root_path, 'data/ccd_defs.json')
 pyopencv_path = os.path.join(superbuild_path, 'install/lib/python2.7/dist-packages')
 sys.path.append(pyopencv_path)
 
-# add opensfm to python path
+# define opensfm path
 opensfm_path = os.path.join(superbuild_path, "src/opensfm")
 
-# add pmvs to python path
+# define pmvs path
 pmvs2_path = os.path.join(superbuild_path, "install/bin/pmvs2")
+
+# define odm modules path
+odm_modules_path = os.path.join(root_path, "build/bin")
 
 # Define supported image extensions
 supported_extensions = {'.jpg','.jpeg','.png'}
