71e9f5be1c8487f15cbd79cd5ab6aeb0348ac147
edgarriba <edgar.riba@gmail.com>
2015-12-01 09:52:14 +0000

    update config and Ecto cmake

--- configure.sh
+++ configure.sh
@@ -25,10 +25,7 @@ sudo apt-get install build-essential \
                      cmake \
                      git \
                      python-pip \
-                     pkg-config \
-                     libavcodec-dev \
-                     libavformat-dev \
-                     libswscale-dev -y
+                     pkg-config -y
 if [ $? -ne 0 ] 
 then
     echo -e "\e[1;31mERROR: \e[39mWhen Installing Required Requisites\e[0m"
@@ -36,11 +33,13 @@ then
 fi
 
 ## Installing Optional Requisites
-echo -e "\e[1;34mInstalling Optional Requisites\e[0;39m"
-sudo apt-get install python-dev \
+echo -e "\e[1;34mInstalling OpenCV Dependencies\e[0;39m"
+sudo apt-get install libgtk2.0-dev \
+                     libavcodec-dev \
+                     libavformat-dev \
+                     libswscale-dev \
+					 python-dev \
                      python-numpy \
-                     python-pyexiv2 \
-                     python-scipy \
                      libtbb2 \
                      libtbb-dev \
                      libjpeg-dev \
@@ -49,45 +48,57 @@ sudo apt-get install python-dev \
                      libjasper-dev \
                      libflann-dev \
                      libproj-dev \
-                     libboost-all-dev \
-					 libboost-python-dev \
 					 libxext-dev \
                      liblapack-dev \
                      libeigen3-dev \
-                     libgtk2.0-dev \
-                     libvtk5-dev \
-                     libgoogle-glog-dev \
-                     libsuitesparse-dev -y
+                     libvtk5-dev -y
 if [ $? -ne 0 ] 
 then
-    echo -e "\e[1;33mWARNING: \e[39mError when Installing Optional Requisites\e[0m"
+    echo -e "\e[1;31mERROR: \e[39mError when Installing Dependencies Requisites\e[0m"
+    exit 1
 fi
-
-## Installing OpenSfM Requisites
-echo -e "\e[1;34mInstalling OpenSfM Dependencies\e[0;39m"
-sudo pip install networkx \
-                 PyYAML \
-                 numpy \
-                 exifread \
-                 gpxpy \
-                 xmltodict
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;33mWARNING: \e[39mError when Installing OpenSfM Dependencies\e[0m"
-fi
-
-## Installing OpenSfM Requisites
-echo -e "\e[1;34mInstalling Ecto Dependencies\e[0;39m"
-sudo pip install catkin-pkg
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;33mWARNING: \e[39mError when Installing Ecto Dependencies\e[0m"
-fi
-
 
 ## Remove libdc1394-22-dev due to python opencv issue
 echo -e "\e[1;34mRemoving libdc1394-22-dev\e[0;39m"
 sudo apt-get remove libdc1394-22-dev
+
+## Installing OpenSfM Requisites
+echo -e "\e[1;34mInstalling OpenSfM Dependencies\e[0;39m"
+sudo apt-get install python-networkx \
+                     libgoogle-glog-dev \
+                     libsuitesparse-dev \
+                     libboost-all-dev \
+					 libboost-python-dev -y
+
+sudo pip install -U PyYAML \
+                    exifread \
+                    gpxpy \
+                    xmltodict
+if [ $? -ne 0 ] 
+then
+    echo -e "\e[1;31mERROR: \e[39mError when Installing OpenSfM Dependencies\e[0m"
+    exit 1
+fi
+
+## Installing Ecto Requisites
+echo -e "\e[1;34mInstalling Ecto Dependencies\e[0;39m"
+sudo pip install -U catkin-pkg
+sudo apt-get install python-empy -y
+if [ $? -ne 0 ] 
+then
+    echo -e "\e[1;31mERROR: \e[39mError when Installing Ecto Dependencies\e[0m"
+    exit 1
+fi
+
+## Installing OpenDroneMap Requisites
+echo -e "\e[1;34mInstalling OpenDroneMap Dependencies\e[0;39m"
+sudo apt-get install python-pyexiv2 \
+                     python-scipy -y
+if [ $? -ne 0 ] 
+then
+    echo -e "\e[1;31mERROR: \e[39mError when Installing OpenDroneMap Dependencies\e[0m"
+    exit 1
+fi
 
 ## Get sys vars
 NUM_CORES=`grep -c processor /proc/cpuinfo`
