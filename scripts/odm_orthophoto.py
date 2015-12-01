ddd40a3435f9d067ca739170a77e4c5c4891fb61
edgarriba <edgar.riba@gmail.com>
2015-12-01 16:26:13 +0000

    add orthophoto cell

--- scripts/odm_orthophoto.py
+++ scripts/odm_orthophoto.py
@@ -1,4 +1,45 @@
-import system
+import ecto
+
+from opendm import io
+from opendm import log
+from opendm import system
+from opendm import context
+
+class ODMOrthoPhotoCell(ecto.Cell):
+
+    def declare_io(self, params, inputs, outputs):
+        inputs.declare("args", "The application arguments.", {})
+
+    def process(self, inputs, outputs):
+
+        log.ODM_INFO('Running OMD OrthoPhoto Cell')
+
+        # get inputs
+        args = self.inputs.args
+        project_path = io.absolute_path_file(args['project_path'])
+
+        # define paths and create working directories
+        odm_texturing = io.join_paths(project_path, 'odm_texturing')
+        odm_orthophoto = io.join_paths(project_path, 'odm_orthophoto')
+        system.mkdir_p(odm_orthophoto)
+
+        # odm_georeference definitions
+        kwargs = {
+            'bin': context.odm_modules_path,
+            'model_geo': io.join_paths(odm_texturing, 'odm_textured_model_geo.obj'),
+            'log': io.join_paths(odm_orthophoto, 'odm_orthophoto_log.txt'),
+            'ortho': io.join_paths(odm_orthophoto, 'odm_orthphoto.png'),
+            'corners': io.join_paths(odm_orthophoto, 'odm_orthphoto_corners.txt'),
+            'res': str(20.0)
+        }
+
+        # run odm_georeference
+        system.run('{bin}/odm_orthophoto -inputFile {model_geo} -logFile {log} ' \
+        '-outputFile {ortho} -resolution {res} -outputCornerFile {corners}'.format(**kwargs))
+
+        log.ODM_INFO('Running OMD OrthoPhoto Cell - Finished')
+        return ecto.OK if args['end_with'] != 'odm_orthophoto' else ecto.QUIT
+
 
 def odm_orthophoto():
     """Run odm_orthophoto"""
