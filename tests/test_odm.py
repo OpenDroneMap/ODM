3959582b6913df0f80e255f239e888702c7d165c
Dakota Benjamin <dakota.benjamin@gmail.com>
2016-02-04 14:06:05 -0500

    Add resize cell and pseudo-code

--- tests/test_odm.py
+++ tests/test_odm.py
@@ -2,6 +2,7 @@ import sys
 from opendm import config
 import ecto
 from scripts.odm_app import ODMApp
+from scripts.resize import ODMResizeCell
 err = sys.stderr
 # Fixture functions
 app = None
@@ -26,3 +27,22 @@ def test_config():
     # check the args
     test_args = 'tests/test_data' # sample test args
     assert test_args == config.args.get('project_path')
+
+
+class TestResize:
+    def __init__(self):
+        # Initialize whatever
+
+    def setUp(self):
+        #call cell
+        resizeCell = ODMResizeCell()
+
+    def tearDown(self):
+        #teardown functions
+
+
+    # assert proper config/args
+
+    # Check images in images_resize
+
+    # Check images are resized (imagemagick?)
--- tests/test_prematching.py
+++ tests/test_prematching.py
@@ -5,32 +5,31 @@ from opendm import system
 
 import sys
 import ecto
-
 from scripts.odm_app import ODMApp
 from scripts.opensfm import ODMOpenSfMCell
 
-def setup_app(self):
-    # # set up test folder
-    # mkdir_p test folder
-    # git clone test_photos.git
-    # # create ODMApp object
-    # app = ODMApp(args=config.args)
+app = None
 
-def teardown_app(self):
-    # Delete the test folder
+def setup_prematching(self):
+    args = {'end_with':'resize','project_path':'tests/test_data'}
+    global app
+    app = ODMApp(args=config.args)
 
+def teardown_prematching(self):
+    # Delete the test folders
+    # if folders exist
+        # delete all but "images" folder
 
-class TestOpenSfM:
-    def setUp(self):
-        # # create opensfm cell
-        # opensfm = ODMOpenSfMCell(use_exif_size=False,
-        #                          feature_process_size=config.args['resize_to'],
-        #                          feature_min_frames=config.args['min_num_features'],
-        #                          processes=context.num_cores,
-        #                          matching_gps_neighbors=config.args['matcher_k']),
+def setUp():
+    # create opensfm cell
+    # opensfm = ODMOpenSfMCell(use_exif_size=False,
+    #                          feature_process_size=config.args['resize_to'],
+    #                          feature_min_frames=config.args['min_num_features'],
+    #                          processes=context.num_cores,
+    #                          matching_gps_neighbors=config.args['matcher_k']),
 
-    def test_prematching_config(self):
-        # assert that relevant run args are what they say they are
+def test_prematching_config():
+    # assert that relevant run args are what they say they are
 
     def test_opensfm_config_file(self):
         # assert that the args i nthe opensfm config file match those it is set to
