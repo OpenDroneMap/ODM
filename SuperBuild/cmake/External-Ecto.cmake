8643b70b5d55a80770697effbc537d7ccddd010a
Dakota Benjamin <dakota.benjamin@gmail.com>
2016-02-19 11:54:19 -0500

    Update ecto download/md5
    Former-commit-id: a74e73a39a2d6d708ad56d1990f8ab1ebc10baab

--- SuperBuild/cmake/External-Ecto.cmake
+++ SuperBuild/cmake/External-Ecto.cmake
@@ -8,8 +8,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-  URL               https://github.com/plasmodic/ecto/archive/master.zip
-  URL_MD5           8cd70c525bcda99d9eba5ac2865e42dd
+  URL               https://github.com/plasmodic/ecto/archive/c6178ed0102a66cebf503a4213c27b0f60cfca69.zip
+  URL_MD5           A5C4757B656D536D3E3CC1DC240EC158
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
