8a0ec303ca7f2d801c19b07d2fbafac86d363c60
Alex Hagiopol <alexhagiopol@gmail.com>
2016-07-20 19:14:22 -0700

    Giant fix for Docker run failure due to lack of Ecto. Configure.sh is not finalized.
    Former-commit-id: 27663156af3296e779f9e4f1c9cbcde1aaebf352

--- configure.sh
+++ configure.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-
 # Check OS
-if [ ! $(command -v apt-get) ]; then
-  echo -e "\e[1;31mERROR: Not a Debian-based linux system. 
-           Impossible to install OpenCV with this script\e[0;39m"
-  exit 1
-fi
+#if [ ! $(command -v apt-get) ]; then
+#  echo -e "\e[1;31mERROR: Not a Debian-based linux system.
+#           Impossible to install OpenCV with this script\e[0;39m"
+#  exit 1
+#fi
 
 ## Before installing
-echo -e "\e[1;34mUpdating the system\e[0;39m"
+#echo -e "\e[1;34mUpdating the system\e[0;39m"
 sudo apt-get update
-END_CMD1=$?
+#END_CMD1=$?
 # sudo apt-get upgrade -y
 # END_CMD2=$?
-if [ $END_CMD1 -ne 0 ]
-then
-	echo -e "\e[1;31mERROR: \e[39mWhen Updating the system\e[0m"
-	exit 1
-fi
+#if [ $END_CMD1 -ne 0 ]
+#then
+#	echo -e "\e[1;31mERROR: \e[39mWhen Updating the system\e[0m"
+#	exit 1
+#fi
 
 ## Install Required Requisites
-echo -e "\e[1;34mInstalling Required Requisites\e[0;39m"
-sudo apt-get install build-essential \
+#echo -e "\e[1;34mInstalling Required Requisites\e[0;39m"
+sudo apt-get install -y build-essential \
                      cmake \
                      git \
                      python-pip \
@@ -30,21 +29,21 @@ sudo apt-get install build-essential \
                      libgeotiff-dev \
                      pkg-config -y -qq
 
-# If we have ubuntu version 14.04, the cmake version in apt-get is too low for mvs-texturing.
-if [[ `lsb_release -rs` == "14.04" ]];
-then
-    bash upgradecmake.sh
-fi
+# Upgrade CMake for mvs-texturing
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
+sudo apt-get update -y
+sudo apt-get install cmake -y
 
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;31mERROR: \e[39mWhen Installing Required Requisites\e[0m"
-    exit 1
-fi
+#if [ $? -ne 0 ]
+#then
+#    echo -e "\e[1;31mERROR: \e[39mWhen Installing Required Requisites\e[0m"
+#    exit 1
+#fi
 
 ## Installing Optional Requisites
-echo -e "\e[1;34mInstalling OpenCV Dependencies\e[0;39m"
-sudo apt-get install libgtk2.0-dev \
+#echo -e "\e[1;34mInstalling OpenCV Dependencies\e[0;39m"
+sudo apt-get install -y libgtk2.0-dev \
                      libavcodec-dev \
                      libavformat-dev \
                      libswscale-dev \
@@ -62,19 +61,19 @@ sudo apt-get install libgtk2.0-dev \
                      liblapack-dev \
                      libeigen3-dev \
                      libvtk5-dev -y -qq
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;31mERROR: \e[39mError when Installing Dependencies Requisites\e[0m"
-    exit 1
-fi
+#if [ $? -ne 0 ]
+#then
+#    echo -e "\e[1;31mERROR: \e[39mError when Installing Dependencies Requisites\e[0m"
+#    exit 1
+#fi
 
 ## Remove libdc1394-22-dev due to python opencv issue
-echo -e "\e[1;34mRemoving libdc1394-22-dev\e[0;39m"
+#echo -e "\e[1;34mRemoving libdc1394-22-dev\e[0;39m"
 sudo apt-get remove libdc1394-22-dev
 
 ## Installing OpenSfM Requisites
-echo -e "\e[1;34mInstalling OpenSfM Dependencies\e[0;39m"
-sudo apt-get install python-networkx \
+#echo -e "\e[1;34mInstalling OpenSfM Dependencies\e[0;39m"
+sudo apt-get install -y python-networkx \
                      libgoogle-glog-dev \
                      libsuitesparse-dev \
                      libboost-filesystem-dev \
@@ -88,45 +87,45 @@ sudo pip install -U PyYAML \
                     exifread \
                     gpxpy \
                     xmltodict
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;31mERROR: \e[39mError when Installing OpenSfM Dependencies\e[0m"
-    exit 1
-fi
+#if [ $? -ne 0 ]
+#then
+#    echo -e "\e[1;31mERROR: \e[39mError when Installing OpenSfM Dependencies\e[0m"
+#    exit 1
+#fi
 
 ## Installing Ecto Requisites
-echo -e "\e[1;34mInstalling Ecto Dependencies\e[0;39m"
+#echo -e "\e[1;34mInstalling Ecto Dependencies\e[0;39m"
 sudo pip install -U catkin-pkg
-sudo apt-get install python-empy \
+sudo apt-get install -y python-empy \
                      python-nose \
                      python-pyside -y -qq
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;31mERROR: \e[39mError when Installing Ecto Dependencies\e[0m"
-    exit 1
-fi
+#if [ $? -ne 0 ]
+#then
+#    echo -e "\e[1;31mERROR: \e[39mError when Installing Ecto Dependencies\e[0m"
+#    exit 1
+#fi
 
 ## Installing OpenDroneMap Requisites
-echo -e "\e[1;34mInstalling OpenDroneMap Dependencies\e[0;39m"
-sudo apt-get install python-pyexiv2 \
+#echo -e "\e[1;34mInstalling OpenDroneMap Dependencies\e[0;39m"
+sudo apt-get install -y python-pyexiv2 \
                      python-scipy \
                      jhead \
                      liblas-bin -y -qq
-if [ $? -ne 0 ] 
-then
-    echo -e "\e[1;31mERROR: \e[39mError when Installing OpenDroneMap Dependencies\e[0m"
-    exit 1
-fi
+#if [ $? -ne 0 ]
+#then
+#    echo -e "\e[1;31mERROR: \e[39mError when Installing OpenDroneMap Dependencies\e[0m"
+#    exit 1
+#fi
 
 ## Get sys vars
-NUM_CORES=`grep -c processor /proc/cpuinfo`
+#NUM_CORES=`grep -c processor /proc/cpuinfo`
 
 ## Add SuperBuild path to the python path
-export PYTHONPATH=$PYTHONPATH:`pwd`/SuperBuild/install/lib/python2.7/dist-packages:`pwd`/SuperBuild/src/opensfm
+#export PYTHONPATH=$PYTHONPATH:`pwd`/SuperBuild/install/lib/python2.7/dist-packages:`pwd`/SuperBuild/src/opensfm
 
 ## Compile SuperBuild
 cd SuperBuild
 mkdir -p build && cd build
-cmake .. && make -j${NUM_CORES}
+cmake .. && make -j8 #${NUM_CORES}
 
-echo -e "\e[1;34mScript finished\e[0;39m"
+#echo -e "\e[1;34mScript finished\e[0;39m"
