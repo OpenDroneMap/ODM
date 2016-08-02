0fd25b554bfa4a1821c2c22e3ecbf06cff90af1c
Piero Toffanin <pt@masseranolabs.com>
2016-08-02 09:11:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ RUNPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=$RUNPATH/SuperBuild/install/lib/python2.7/dist-packages:$RUNPATH/SuperBuild/src/opensfm:$PYTHONPATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RUNPATH/SuperBuild/install/lib
 bash configure.sh
-mkdir build && cd build && cmake .. && make && cd ..
