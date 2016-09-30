cff1f6e75a307b3b8f479df315a50a386edcd168
Seth Fitzsimmons <seth@mojodna.net>
2016-09-30 02:42:10 -0700

    Use multiprocessing to parallelize early tasks
    Former-commit-id: 9beb0cc2e5fc977c5d8de99dd544dbab70fef239

--- scripts/resize.py
+++ scripts/resize.py
@@ -2,10 +2,59 @@ import ecto
 import cv2
 import pyexiv2
 
+from functools import partial
+from multiprocessing import Pool
 from opendm import log
 from opendm import system
 from opendm import io
 from opendm import types
+
+
+def resize(src_dir, target_dir, resize_to, rerun_cell, photo):
+    # define image paths
+    path_file = photo.path_file
+    new_path_file = io.join_paths(target_dir, photo.filename)
+    # set raw image path in case we want to rerun cell
+    if io.file_exists(new_path_file) and rerun_cell:
+        path_file = io.join_paths(src_dir, photo.filename)
+
+    if not io.file_exists(new_path_file) or rerun_cell:
+        # open and resize image with opencv
+        img = cv2.imread(path_file)
+        # compute new size
+        max_side = max(img.shape[0], img.shape[1])
+        if max_side <= resize_to:
+            log.ODM_WARNING('Resize Parameter is greater than the largest side of the image')
+        ratio = float(resize_to) / float(max_side)
+        img_r = cv2.resize(img, None, fx=ratio, fy=ratio)
+        # write image with opencv
+        cv2.imwrite(new_path_file, img_r)
+        # read metadata with pyexiv2
+        old_meta = pyexiv2.ImageMetadata(path_file)
+        new_meta = pyexiv2.ImageMetadata(new_path_file)
+        old_meta.read()
+        new_meta.read()
+        # copy metadata
+        old_meta.copy(new_meta)
+        # update metadata size
+        new_meta['Exif.Photo.PixelXDimension'] = img_r.shape[0]
+        new_meta['Exif.Photo.PixelYDimension'] = img_r.shape[1]
+        new_meta.write()
+        # update photos array with new values
+        photo.path_file = new_path_file
+        photo.width = img_r.shape[0]
+        photo.height = img_r.shape[1]
+        photo.update_focal()
+
+        # log message
+        log.ODM_DEBUG('Resized %s | dimensions: %s' %
+                      (photo.filename, img_r.shape))
+    else:
+        # log message
+        log.ODM_WARNING('Already resized %s | dimensions: %s x %s' %
+                        (photo.filename, photo.width, photo.height))
+
+    return photo
 
 
 class ODMResizeCell(ecto.Cell):
@@ -51,49 +100,14 @@ class ODMResizeCell(ecto.Cell):
                       'resize' in args.rerun_from)
 
         # loop over photos
-        for photo in photos:
-            # define image paths
-            path_file = photo.path_file
-            new_path_file = io.join_paths(tree.dataset_resize, photo.filename)
-            # set raw image path in case we want to rerun cell
-            if io.file_exists(new_path_file) and rerun_cell:
-                path_file = io.join_paths(tree.dataset_raw, photo.filename)
-
-            if not io.file_exists(new_path_file) or rerun_cell:
-                # open and resize image with opencv
-                img = cv2.imread(path_file)
-                # compute new size
-                max_side = max(img.shape[0], img.shape[1])
-                if max_side <= self.params.resize_to:
-                    log.ODM_WARNING('Resize Parameter is greater than the largest side of the image')
-                ratio = float(self.params.resize_to) / float(max_side)
-                img_r = cv2.resize(img, None, fx=ratio, fy=ratio)
-                # write image with opencv
-                cv2.imwrite(new_path_file, img_r)
-                # read metadata with pyexiv2
-                old_meta = pyexiv2.ImageMetadata(path_file)
-                new_meta = pyexiv2.ImageMetadata(new_path_file)
-                old_meta.read()
-                new_meta.read()
-                # copy metadata
-                old_meta.copy(new_meta)
-                # update metadata size
-                new_meta['Exif.Photo.PixelXDimension'] = img_r.shape[0]
-                new_meta['Exif.Photo.PixelYDimension'] = img_r.shape[1]
-                new_meta.write()
-                # update photos array with new values
-                photo.path_file = new_path_file
-                photo.width = img_r.shape[0]
-                photo.height = img_r.shape[1]
-                photo.update_focal()
-
-                # log message
-                log.ODM_DEBUG('Resized %s | dimensions: %s' %
-                              (photo.filename, img_r.shape))
-            else:
-                # log message
-                log.ODM_WARNING('Already resized %s | dimensions: %s x %s' %
-                                (photo.filename, photo.width, photo.height))
+        photos = Pool().map(
+            partial(resize,
+                    tree.dataset_raw,
+                    tree.dataset_resize,
+                    self.params.resize_to,
+                    rerun_cell),
+            photos
+        )
 
         log.ODM_INFO('Resized %s images' % len(photos))
 
@@ -105,4 +119,3 @@ class ODMResizeCell(ecto.Cell):
 
         log.ODM_INFO('Running ODM Resize Cell - Finished')
         return ecto.OK if args.end_with != 'resize' else ecto.QUIT
-
