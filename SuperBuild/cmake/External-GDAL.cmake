0d7c64238781a9df69457d2c187e29b371ffe853
Stephen Vincent Mather <stephen@smathermather.com>
2017-02-23 20:40:06 +0000

    use custom gdal build
    Former-commit-id: ed6d7ef7ff16dfb7f3229220f7920b126dfdcf0d

---
+++ SuperBuild/cmake/External-GDAL.cmake
@@ -0,0 +1,25 @@
+set(_proj_name pdal)
+set(_SB_BINARY_DIR "${SB_BINARY_DIR}/${_proj_name}")
+
+ExternalProject_Add(${_proj_name}
+  PREFIX            ${_SB_BINARY_DIR}
+  TMP_DIR           ${_SB_BINARY_DIR}/tmp
+  STAMP_DIR         ${_SB_BINARY_DIR}/stamp
+  #--Download step--------------
+  DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
+  URL               http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz 
+  URL_MD5           e5057f6011b720cfbbebbbbf216c0ffe 
+  #--Update/Patch step----------
+  UPDATE_COMMAND    ""
+  #--Configure step-------------
+  SOURCE_DIR        ${SB_SOURCE_DIR}/${_proj_name}
+  CMAKE_ARGS
+  #--Build step-----------------
+  BINARY_DIR        ${_SB_BINARY_DIR}
+  #--Install step---------------
+  INSTALL_DIR       ${SB_INSTALL_DIR}
+  #--Output logging-------------
+  LOG_DOWNLOAD      OFF
+  LOG_CONFIGURE     OFF
+  LOG_BUILD         OFF
+)
