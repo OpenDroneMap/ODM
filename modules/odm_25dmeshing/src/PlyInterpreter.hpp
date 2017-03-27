c20d5dcbde332fbc55c8f042b8de2ff6f1399bcb
Piero Toffanin <pt@masseranolabs.com>
2017-03-27 16:41:15 -0400

    Added Odm25dMeshing module, CGAL read Ply file example
    Former-commit-id: 305cb4a6aa0d87843c57df0e037dabc605122751

---
+++ modules/odm_25dmeshing/src/PlyInterpreter.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+#include <fstream>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/property_map.h>
+#include <CGAL/IO/read_ply_points.h>
+
+// types
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::FT FT;
+typedef Kernel::Point_3 Point;
+typedef Kernel::Vector_3 Vector;
+
+// Point with normal vector stored as a std::pair.
+typedef std::pair<Point, Vector> Pwn;
+// Color is red/green/blue array
+typedef CGAL::cpp11::array<unsigned char, 3> Color;
+
+class PlyInterpreter {
+	 std::vector<Pwn>& points;
+	 std::vector<Color>& colors;
+
+	public:
+	 PlyInterpreter (std::vector<Pwn>& points,
+			    std::vector<Color>& colors)
+	    : points (points), colors (colors)
+	  { }
+	  bool is_applicable (CGAL::Ply_reader& reader);
+	  void process_line (CGAL::Ply_reader& reader);
+};
