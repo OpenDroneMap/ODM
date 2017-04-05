82ebd04c03087fb8a04af5bb45812c45a2254b36
Piero Toffanin <pt@masseranolabs.com>
2017-04-05 09:41:22 -0400

    Got odm_25dmeshing to compile under OpenDroneMap env
    Former-commit-id: 4cef227a6451c4c8d86d84aa6114c6648505b172

--- configure.sh
+++ configure.sh
@@ -20,8 +20,7 @@ sudo apt-get install -y -qq build-essential \
                      libgdal-dev \
                      gdal-bin \
                      libgeotiff-dev \
-                     pkg-config \
-                     libcgal-dev
+                     pkg-config
 
 echo "Getting CMake 3.1 for MVS-Texturing"
 sudo apt-get install -y software-properties-common python-software-properties
