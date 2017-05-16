66ff8161eb1492c645396fcc5667a8d7fe1334d0
Pau Gargallo <pau.gargallo@gmail.com>
2017-05-16 15:38:12 +0200

    Add scripts to setup/split/reconstruct/align a metadataset
    Former-commit-id: 3c20a2508498c7080d92ae82cb1703754b507a01

---
+++ scripts/metadataset/split.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import subprocess
+
+from opendm import context
+
+
+def run_command(args):
+    result = subprocess.Popen(args).wait()
+    if result != 0:
+        raise RuntimeError(result)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Split metadaset into submodels')
+    parser.add_argument('dataset',
+                        help='path to the dataset to be processed')
+    args = parser.parse_args()
+
+    command = os.path.join(context.opensfm_path, 'bin', 'opensfm')
+    path = os.path.join(args.dataset, 'opensfm')
+
+    run_command([command, 'create_submodels', path, '--size', '20', '--dist', '20'])
