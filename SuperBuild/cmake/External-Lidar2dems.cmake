009b58be34209dbc0f138c97b9d503500228d9c6
Piero Toffanin <pt@masseranolabs.com>
2017-07-06 14:39:58 -0400

    Removed MD5 check for lidar2dems

--- SuperBuild/cmake/External-Lidar2dems.cmake
+++ SuperBuild/cmake/External-Lidar2dems.cmake
@@ -2,14 +2,12 @@ set(_proj_name lidar2dems)
 set(_SB_BINARY_DIR "${SB_BINARY_DIR}/${_proj_name}")
 
 ExternalProject_Add(${_proj_name}
-  DEPENDS           opensfm
   PREFIX            ${_SB_BINARY_DIR}
   TMP_DIR           ${_SB_BINARY_DIR}/tmp
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
   URL               https://github.com/OpenDroneMap/lidar2dems/archive/master.zip
-  URL_MD5           76ae5d0182f280bec890bdfd156aa538
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
