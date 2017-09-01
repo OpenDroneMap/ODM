d8cecc02154c9b178098555bff50a3169c9a5f45
Dakota Benjamin <dmb2@clevelandmetroparks.com>
2017-09-01 11:31:22 -0400

    Use an updated version of OpenSfM for better memory management
    Former-commit-id: 71ce34662e14eb976752a42f1b8d9b301dca15b5

--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -8,8 +8,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  URL               https://github.com/mapillary/OpenSfM/archive/v0.1.0.tar.gz
-  URL_MD5           0C46922C61F7CA54D272FBB4CC7FFBBB
+  URL               https://github.com/mapillary/OpenSfM/archive/0a5a6f07b66088e9b63bddaf29bb8f49dd39f9e4.zip
+  URL_MD5           2ed2c4a31801ddd57740261aaaf97ac9
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
