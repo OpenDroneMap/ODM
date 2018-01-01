bf97356bb285c1f5ecc735f786e792a65eed1b78
Piero Toffanin <pt@masseranolabs.com>
2018-01-01 15:06:09 -0500

    Removed 2.5D mesh module dependency on PCL, multithread support, removed CGAL, added VTK7 module
    Former-commit-id: 778e5ad6290c0552db21a4af117e56026c34d366

--- SuperBuild/cmake/External-VTK7.cmake
+++ SuperBuild/cmake/External-VTK7.cmake
@@ -1,4 +1,4 @@
-set(_proj_name cgal)
+set(_proj_name vtk7)
 set(_SB_BINARY_DIR "${SB_BINARY_DIR}/${_proj_name}")
 
 ExternalProject_Add(${_proj_name}
@@ -7,14 +7,18 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-  URL               https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.9/CGAL-4.9.zip
-  URL_MD5           31c08d762a72fda785df194c89b833df
+  URL               https://github.com/Kitware/VTK/archive/v7.1.1.zip
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
   SOURCE_DIR        ${SB_SOURCE_DIR}/${_proj_name}
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
+    -DVTK_SMP_IMPLEMENTATION_TYPE=TBB 
+    -DCMAKE_BUILD_TYPE=Release 
+    -DBUILD_SHARED_LIBS=OFF 
+    -DVTK_Group_Rendering=OFF 
+    -DBUILD_TESTING=OFF
   #--Build step-----------------
   BINARY_DIR        ${_SB_BINARY_DIR}
   #--Install step---------------
