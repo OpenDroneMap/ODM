1bd40c0dea5d61ea02c4f0c07abce9b06ffc78f6
Piero Toffanin <pt@masseranolabs.com>
2018-03-25 10:25:25 -0400

    Modified odm_georef to preserve double coordinates precision when applying transform to point cloud

--- modules/odm_georef/src/modifiedPclFunctions.hpp
+++ modules/odm_georef/src/modifiedPclFunctions.hpp
@@ -7,7 +7,6 @@
 // PCL
 #include <pcl/point_types.h>
 #include <pcl/surface/texture_mapping.h>
-#include <pcl/io/ply_io.h>
 #include <pcl/io/obj_io.h>
 
 int saveOBJFile(const std::string &file_name, const pcl::TextureMesh &tex_mesh, unsigned precision);
