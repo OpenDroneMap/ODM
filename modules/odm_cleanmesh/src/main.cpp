4d13d1a2d4fe9089669e40eead3912aede137d4d
Piero Toffanin <pt@masseranolabs.com>
2018-06-12 11:56:17 -0400

    Decimation parameters, island removal optional
    Former-commit-id: e0c2b16a6650ad2c6b4cfb2954f8728e12eddd20

--- modules/odm_cleanmesh/src/main.cpp
+++ modules/odm_cleanmesh/src/main.cpp
@@ -5,22 +5,24 @@
 #include <vtkSmartPointer.h>
 #include <vtkPLYReader.h>
 #include <vtkPLYWriter.h>
+#include <vtkAlgorithmOutput.h>
+#include <vtkQuadricDecimation.h>
 #include "CmdLineParser.h"
 #include "Logger.h"
-
-#define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
 
 Logger logWriter;
 
 cmdLineParameter< char* >
     InputFile( "inputFile" ) ,
-	OutputFile( "outputFile" );
+    OutputFile( "outputFile" );
+cmdLineParameter< int >
+    DecimateMesh( "decimateMesh" );
 cmdLineReadable
+    RemoveIslands( "removeIslands" ) ,
 	Verbose( "verbose" );
 
-cmdLineReadable* params[] =
-{
-    &InputFile , &OutputFile , &Verbose ,
+cmdLineReadable* params[] = {
+    &InputFile , &OutputFile , &DecimateMesh, &RemoveIslands, &Verbose ,
     NULL
 };
 
@@ -33,12 +35,15 @@ void help(char *ex){
     std::cout << "Usage: " << ex << std::endl
               << "\t -" << InputFile.name << " <input polygon mesh>" << std::endl
               << "\t -" << OutputFile.name << " <output polygon mesh>" << std::endl
+              << "\t [-" << DecimateMesh.name << " <target number of polygons>]" << std::endl
+              << "\t [-" << RemoveIslands.name << "]" << std::endl
+
               << "\t [-" << Verbose.name << "]" << std::endl;
     exit(EXIT_FAILURE);
 }
 
 
-void logParams(){
+void logArgs(cmdLineReadable* params[], Logger& logWriter){
     logWriter("Running with parameters:\n");
     char str[1024];
     for( int i=0 ; params[i] ; i++ ){
@@ -53,28 +58,59 @@ void logParams(){
 
 int main(int argc, char **argv) {
     cmdLineParse( argc-1 , &argv[1] , params );
-    if( !InputFile.set || !OutputFile.set ) help( argv[0] );
+    if( !InputFile.set || !OutputFile.set ) help(argv[0]);
+    if( !RemoveIslands.set && !DecimateMesh.set ) help (argv[0]);
+
 
     logWriter.verbose = Verbose.set;
     logWriter.outputFile = "odm_cleanmesh_log.txt";
-    logParams();
+    logArgs(params, logWriter);
 
 	vtkSmartPointer<vtkPLYReader> reader =
     vtkSmartPointer<vtkPLYReader>::New();
         reader->SetFileName ( InputFile.value );
+    reader->Update();
 
-    vtkSmartPointer<vtkPolyDataConnectivityFilter> connectivityFilter = 
+    vtkPolyData *nextOutput = reader->GetOutput();
+
+    vtkSmartPointer<vtkPolyDataConnectivityFilter> connectivityFilter =
     vtkSmartPointer<vtkPolyDataConnectivityFilter>::New();
-	connectivityFilter->SetInputConnection(reader->GetOutputPort());
-	connectivityFilter->SetExtractionModeToLargestRegion(); 
+    connectivityFilter->SetExtractionModeToLargestRegion();
+
+    vtkSmartPointer<vtkQuadricDecimation> decimationFilter =
+    vtkSmartPointer<vtkQuadricDecimation>::New();
+
+    if (RemoveIslands.set){
+        logWriter("Removing islands\n");
+        connectivityFilter->SetInputData(nextOutput);
+        connectivityFilter->Update();
+        nextOutput = connectivityFilter->GetOutput();
+    }
+
+    if (DecimateMesh.set){
+        logWriter("Decimating mesh\n");
+
+        int vertexCount = nextOutput->GetNumberOfPoints();
+        logWriter("Current vertex count: %d\n", vertexCount);
+        logWriter("Wanted vertex count: %d\n", DecimateMesh.value);
+
+        if (vertexCount > DecimateMesh.value){
+            decimationFilter->SetTargetReduction(static_cast<double>(DecimateMesh.value) / static_cast<double>(nextOutput->GetNumberOfPolys()));
+            decimationFilter->SetInputData(nextOutput);
+            decimationFilter->Update();
+            nextOutput = decimationFilter->GetOutput();
+        }else{
+            logWriter("Skipping decimation\n");
+        }
+    }
 
     logWriter("Saving cleaned mesh to file... \n");
 
     vtkSmartPointer<vtkPLYWriter> plyWriter =
         vtkSmartPointer<vtkPLYWriter>::New();
     plyWriter->SetFileName(OutputFile.value);
-    plyWriter->SetInputConnection(connectivityFilter->GetOutputPort());
     plyWriter->SetFileTypeToBinary();
+    plyWriter->SetInputData(nextOutput);
     plyWriter->Write();
 
     logWriter("OK\n");
