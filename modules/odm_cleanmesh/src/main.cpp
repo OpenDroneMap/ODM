38325a5d2c4959a205d9300db3ad221bdaab85f8
Piero Toffanin <pt@masseranolabs.com>
2018-06-12 10:00:34 -0400

    Better command line parsing, help
    Former-commit-id: e11295be563ee78a0339c345c38ef48c4d8ad371

--- modules/odm_cleanmesh/src/main.cpp
+++ modules/odm_cleanmesh/src/main.cpp
@@ -5,91 +5,77 @@
 #include <vtkSmartPointer.h>
 #include <vtkPLYReader.h>
 #include <vtkPLYWriter.h>
+#include "CmdLineParser.h"
+#include "Logger.h"
 
 #define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
 
-std::string inputFile = "", outputFile = "";
-bool verbose = false;
+Logger logWriter;
+
+cmdLineParameter< char* >
+    InputFile( "inputFile" ) ,
+	OutputFile( "outputFile" );
+cmdLineReadable
+	Verbose( "verbose" );
+
+cmdLineReadable* params[] =
+{
+    &InputFile , &OutputFile , &Verbose ,
+    NULL
+};
 
 // TODO:
-// 1. better help
 // 2. optional decimation filter
 // 3. optional island removal
-// 4. exit macros
 
-void help(){
-	std::cout << "HELP TODO" << std::endl;
-	// log << "Usage: odm_25dmeshing -inputFile [plyFile] [optional-parameters]\n";
-	// log << "Create a 2.5D mesh from a point cloud. "
-	// 	<< "The program requires a path to an input PLY point cloud file, all other input parameters are optional.\n\n";
 
-	// log << "	-inputFile	<path>	to PLY point cloud\n"
-	// 	<< "	-outputFile	<path>	where the output PLY 2.5D mesh should be saved (default: " << outputFile << ")\n"
-	// 	<< "	-outputDsmFile	<path>	Optionally output the Digital Surface Model (DSM) computed for generating the mesh. (default: " << outputDsmFile << ")\n"
-	// 	<< "	-logFile	<path>	log file path (default: " << logFilePath << ")\n"
-	// 	<< "	-verbose	whether to print verbose output (default: " << (printInCoutPop ? "true" : "false") << ")\n"
-	// 	<< "	-maxVertexCount	<0 - N>	Maximum number of vertices in the output mesh. The mesh might have fewer vertices, but will not exceed this limit. (default: " << maxVertexCount << ")\n"
-	// 	<< "	-neighbors	<1 - 1000>	Number of nearest neighbors to consider when doing shepard's interpolation and outlier removal. Higher values lead to smoother meshes but take longer to process. (default: " << neighbors << ")\n"
-	// 	<< "	-resolution	<0 - N>	Size of the interpolated digital surface model (DSM) used for deriving the 2.5D mesh, expressed in pixels per meter unit. When set to zero, the program automatically attempts to find a good value based on the point cloud extent and target vertex count. (default: " << resolution << ")\n"
-
-	// 	<< "\n";
-	exit(0);
+void help(char *ex){
+    std::cout << "Usage: " << ex << std::endl
+              << "\t -" << InputFile.name << " <input polygon mesh>" << std::endl
+              << "\t -" << OutputFile.name << " <output polygon mesh>" << std::endl
+              << "\t [-" << Verbose.name << "]" << std::endl;
+    exit(EXIT_FAILURE);
 }
 
-void parseArguments(int argc, char **argv) {
-	for (int argIndex = 1; argIndex < argc; ++argIndex) {
-		// The argument to be parsed.
-		std::string argument = std::string(argv[argIndex]);
 
-		if (argument == "-help") {
-			help();
-			exit(0);
-		} else if (argument == "-verbose") {
-			verbose = true;
-		} else if (argument == "-inputFile" && argIndex < argc) {
-			++argIndex;
-			if (argIndex >= argc) {
-				throw std::runtime_error(
-						"Argument '" + argument
-								+ "' expects 1 more input following it, but no more inputs were provided.");
-			}
-			inputFile = std::string(argv[argIndex]);
-		} else if (argument == "-outputFile" && argIndex < argc) {
-			++argIndex;
-			if (argIndex >= argc) {
-				throw std::runtime_error(
-						"Argument '" + argument
-								+ "' expects 1 more input following it, but no more inputs were provided.");
-			}
-			outputFile = std::string(argv[argIndex]);
-		} else {
-			help();
-		}
-	}
+void logParams(){
+    logWriter("Running with parameters:\n");
+    char str[1024];
+    for( int i=0 ; params[i] ; i++ ){
+        if( params[i]->set ){
+            params[i]->writeValue( str );
+            if( strlen( str ) ) logWriter( "\t--%s %s\n" , params[i]->name , str );
+            else                logWriter( "\t--%s\n" , params[i]->name );
+        }
+    }
 }
 
 
 int main(int argc, char **argv) {
-	parseArguments(argc, argv);
-	if (inputFile.empty() || outputFile.empty()) help();
+    cmdLineParse( argc-1 , &argv[1] , params );
+    if( !InputFile.set || !OutputFile.set ) help( argv[0] );
+
+    logWriter.verbose = Verbose.set;
+    logWriter.outputFile = "odm_cleanmesh_log.txt";
+    logParams();
 
 	vtkSmartPointer<vtkPLYReader> reader =
     vtkSmartPointer<vtkPLYReader>::New();
-  		reader->SetFileName ( inputFile.c_str() );
+        reader->SetFileName ( InputFile.value );
 
     vtkSmartPointer<vtkPolyDataConnectivityFilter> connectivityFilter = 
     vtkSmartPointer<vtkPolyDataConnectivityFilter>::New();
 	connectivityFilter->SetInputConnection(reader->GetOutputPort());
 	connectivityFilter->SetExtractionModeToLargestRegion(); 
 
-    std::cout << "Saving cleaned mesh to file... " << std::endl;
+    logWriter("Saving cleaned mesh to file... \n");
 
     vtkSmartPointer<vtkPLYWriter> plyWriter =
         vtkSmartPointer<vtkPLYWriter>::New();
-    plyWriter->SetFileName(outputFile.c_str());
+    plyWriter->SetFileName(OutputFile.value);
     plyWriter->SetInputConnection(connectivityFilter->GetOutputPort());
     plyWriter->SetFileTypeToBinary();
     plyWriter->Write();
 
-	std::cout << "OK" << std::endl;	
+    logWriter("OK\n");
 }
