3e962dceb62aca3726a170c334726d641f440eb4
Dakota Benjamin <dakota.benjamin@gmail.com>
2018-06-30 19:36:53 -0400

    Expose more SMVS parameters

--- scripts/smvs.py
+++ scripts/smvs.py
@@ -8,7 +8,13 @@ from opendm import context
 
 class ODMSmvsCell(ecto.Cell):
     def declare_params(self, params):
+        params.declare("threads", "max number of threads", context.num_cores)
+        params.declare("alpha", "Regularization parameter", 1)
         params.declare("scale", "input scale", 1)
+        params.declare("output_scale", "scale of optimization", 2)
+        params.declare("shading", "Enable shading-aware model", False)
+        params.declare("gamma_srgb", "Apply inverse SRGB gamma correction", False)
+        params.declare("verbose", "Increase debug level", False)
 
     def declare_io(self, params, inputs, outputs):
         inputs.declare("tree", "Struct with paths", [])
@@ -33,9 +39,6 @@ class ODMSmvsCell(ecto.Cell):
             log.ODM_ERROR('Not enough photos in photos array to start SMVS')
             return ecto.QUIT
 
-        # create working directories
-        system.mkdir_p(tree.smvs)
-
         # check if we rerun cell or not
         rerun_cell = (args.rerun is not None and
                       args.rerun == 'smvs') or \
@@ -45,6 +48,7 @@ class ODMSmvsCell(ecto.Cell):
 
         # check if reconstruction was done before
         if not io.file_exists(tree.smvs_model) or rerun_cell:
+
             # make bundle directory
             if not io.file_exists(tree.mve_bundle):
                 system.mkdir_p(tree.mve_path) #TODO: check permissions/what happens when rerun
@@ -52,18 +56,29 @@ class ODMSmvsCell(ecto.Cell):
                 io.copy(tree.opensfm_image_list, tree.mve_image_list)
                 io.copy(tree.opensfm_bundle, tree.mve_bundle)
 
+            # run mve makescene
+            if not io.dir_exists(tree.mve_path):
+                system.run('%s %s %s' % (context.makescene_path, tree.mve_path, tree.smvs))
+
             # config
             config = [
-                "-s%s" % self.params.scale
+                "-t%s" % self.params.threads,
+                "-a%s" % self.params.alpha,
+                "-s%s" % self.params.scale,
+                "-o%s" % self.params.output_scale,
+                "--debug-lvl=%s" % '1' if self.params.verbose else '0',
+                "%s" % '-S' if self.params.shading else '',
+                "%s" % '-g' if self.params.gamma_srgb and self.params.shading else '',
+                "--force" if rerun_cell else ''
             ]
-
-            # run mve makescene
-            system.run('%s %s %s' % (context.makescene_path, tree.mve_path, tree.smvs))
 
             # run smvs
             system.run('%s %s %s' % (context.smvs_path, ' '.join(config), tree.smvs))
             # rename the file for simplicity
-            io.rename_file(io.join_paths(tree.smvs, 'smvs-B%s.ply' % self.params.scale), tree.smvs_model)
+            old_file = io.join_paths(tree.smvs, 'smvs-%s%s.ply' %
+                ('S' if self.params.shading else 'B', self.params.scale))
+            if not (io.rename_file(old_file, tree.smvs_model)):
+                log.ODM_WARNING("File %s does not exist, cannot be renamed. " % old_file)
 
 
         else:
