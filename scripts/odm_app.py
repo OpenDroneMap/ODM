ef02d605c69da942a097f99885ebffae9f290567
India Johnson <itj@clevelandmetroparks.com>
2019-02-11 17:10:36 +0000

    add scale arg mve_output_scale

--- scripts/odm_app.py
+++ scripts/odm_app.py
@@ -50,8 +50,7 @@ class ODMApp(ecto.BlackBox):
                                            hybrid_bundle_adjustment=p.args.use_hybrid_bundle_adjustment),
                  'slam': ODMSlamCell(),
                  
-                 'mve': ODMMveCell(mve_output_scale=p.args.mve_output_scale,
-				   threads=p.args.max_concurrency),
+                 'mve': ODMMveCell(mve_output_scale=p.args.mve_output_scale),
                  
                  'meshing': ODMeshingCell(max_vertex=p.args.mesh_size,
                                           oct_tree=p.args.mesh_octree_depth,
