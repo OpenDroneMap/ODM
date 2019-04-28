5a8284c5cc514696fd15b432dbca23a00e28714c
Piero Toffanin <pt@masseranolabs.com>
2019-04-28 09:49:46 -0400

    Number of lines computation

--- opendm/cutline.py
+++ opendm/cutline.py
@@ -3,16 +3,28 @@ import shutil
 from opendm import log
 from opendm import io
 from opendm import concurrency 
-
+from opendm import get_image_size
+import math
 
 def compute_cutline(orthophoto_file, crop_area_file, destination, max_concurrency=1):
     if io.file_exists(orthophoto_file) and io.file_exists(crop_area_file):
         from opendm.grass_engine import grass
         log.ODM_DEBUG("Computing cutline")
 
+        try:
+            ortho_width,ortho_height = get_image_size.get_image_size(orthophoto_file)
+            log.ODM_DEBUG("Orthophoto dimensions are %sx%s" % (ortho_width, ortho_height))
+            number_lines = int(max(8, math.ceil(min(ortho_width, ortho_height) / 256.0)))
+        except get_image_size.UnknownImageFormat:
+            log.ODM_DEBUG("Cannot compute orthophoto dimensions, setting arbitrary number of lines.")
+            number_lines = 32
+        
+        log.ODM_DEBUG("Number of lines: %s" % number_lines)
+
         gctx = grass.create_context({'auto_cleanup' : False})
         gctx.add_param('orthophoto_file', orthophoto_file)
         gctx.add_param('crop_area_file', crop_area_file)
+        gctx.add_param('number_lines', number_lines)
         gctx.add_param('max_concurrency', max_concurrency)
         gctx.add_param('memory', int(concurrency.get_max_memory_mb(300)))
         gctx.set_location(orthophoto_file)
