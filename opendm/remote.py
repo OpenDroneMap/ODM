a08710ed7c6bee6e20e2d3dd9a7ff52e85e50354
Piero Toffanin <pt@masseranolabs.com>
2019-05-06 14:17:23 -0400

    pyodm version fixed, skip merge point cloud on failure
    Former-commit-id: b0480ae1569ecbbe6520186e212bfcaea45739fc

--- opendm/remote.py
+++ opendm/remote.py
@@ -1,17 +1,33 @@
 from opendm import log
+from pyodm import Node, exceptions
 
 class HybridDistributedExecutor:
     def __init__(self, nodeUrl):
-        self.nodeUrl = nodeUrl
-        log.ODM_INFO("Initializing hybrid distributed executor with cluster node: %s" % nodeUrl)
+        self.node = Node.from_url(nodeUrl)
+
+        log.ODM_INFO("Initializing hybrid distributed executor using cluster node %s" % nodeUrl)
+        try:
+            odm_version = self.node.info().odm_version
+            log.ODM_INFO("Node is online and running ODM version: %s"  % odm_version)
+        except exceptions.NodeConnectionError:
+            log.ODM_WARNING("The node seems to be offline! We'll still process the dataset, but it's going to run entirely locally.")
+        except Exception as e:
+            log.ODM_ERROR("An unexpected problem happened while opening the node connection: %s" % str(e))
+            exit(1)
 
     def set_projects(self, paths):
         self.project_paths = paths
 
-    def run_reconstruct(self):
-        print(self.project_paths)
-        exit(1)
+    def run_reconstruction(self):
+        if not self.project_paths:
+            return
+        
+        
+
+
 
     def run_toolchain(self):
-        pass
-    
+        if not self.project_paths:
+            return
+        
+
