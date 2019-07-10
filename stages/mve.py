3a08c3d97f001302b36bc4ab6db0fff59e9dc582
Piero Toffanin <pt@masseranolabs.com>
2019-07-10 13:37:55 -0400

    Fix invalid quotes bug

--- stages/mve.py
+++ stages/mve.py
@@ -119,7 +119,7 @@ class ODMMveStage(types.ODM_Stage):
             retry_count = 1
             while retry_count < 10:
                 try:
-                    system.run('%s "%s" "%s"' % (context.dmrecon_path, ' '.join(dmrecon_config), tree.mve), env_vars={'OMP_NUM_THREADS': args.max_concurrency})
+                    system.run('%s %s "%s"' % (context.dmrecon_path, ' '.join(dmrecon_config), tree.mve), env_vars={'OMP_NUM_THREADS': args.max_concurrency})
                     break
                 except Exception as e:
                     if str(e) == "Child returned 134" or str(e) == "Child returned 1":
