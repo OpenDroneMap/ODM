877fc96d95ec18621446557216dff1662c5719f6
Piero Toffanin <pt@masseranolabs.com>
2019-09-07 14:52:55 -0400

    Reverted PDAL to 1.9.1

--- SuperBuild/cmake/External-PDAL.cmake
+++ SuperBuild/cmake/External-PDAL.cmake
@@ -8,7 +8,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  URL               https://github.com/PDAL/PDAL/archive/2.0.1.zip
+  URL               https://github.com/PDAL/PDAL/archive/1.9.1.zip
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
