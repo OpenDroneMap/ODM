5480ae53ce144d8e34fb0a54e5b5d9b34ee648b8
Bhumika Dhingra <51711116+bhumikadhingra@users.noreply.github.com>
2019-10-06 18:33:09 +0530

    Update External-Entwine.cmake

--- SuperBuild/cmake/External-Entwine.cmake
+++ SuperBuild/cmake/External-Entwine.cmake
@@ -6,12 +6,12 @@ ExternalProject_Add(${_proj_name}
   PREFIX            ${_SB_BINARY_DIR}
   TMP_DIR           ${_SB_BINARY_DIR}/tmp
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
-  #--Download step--------------
+  #--Download step--------------------------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
   URL               https://github.com/connormanning/entwine/archive/01ff206ca15c5001150f3de9fb202491c388e63c.zip
-  #--Update/Patch step----------
+  #--Update/Patch step-----------------------------
   UPDATE_COMMAND    ""
-  #--Configure step-------------
+  #--Configure step--------------------------------
   SOURCE_DIR        ${SB_SOURCE_DIR}/${_proj_name}
   CMAKE_ARGS
     -DCMAKE_CXX_FLAGS=-isystem\ ${SB_SOURCE_DIR}/pdal
@@ -19,11 +19,11 @@ ExternalProject_Add(${_proj_name}
     -DWITH_TESTS=OFF
 	-DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
-  #--Build step-----------------
+  #--Build step------------------------------------
   BINARY_DIR        ${_SB_BINARY_DIR}
-  #--Install step---------------
+  #--Install step----------------------------------
   INSTALL_DIR       ${SB_INSTALL_DIR}
-  #--Output logging-------------
+  #--Output logging--------------------------------
   LOG_DOWNLOAD      OFF
   LOG_CONFIGURE     OFF
   LOG_BUILD         OFF
