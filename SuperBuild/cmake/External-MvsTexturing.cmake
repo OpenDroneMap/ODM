6226b6c1c7017070473d4f9a9750f15a2423bc4d
Piero Toffanin <pt@masseranolabs.com>
2019-10-23 20:44:01 -0400

    Added start-dev-env.sh script

--- SuperBuild/cmake/External-MvsTexturing.cmake
+++ SuperBuild/cmake/External-MvsTexturing.cmake
@@ -8,7 +8,10 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-  URL               https://github.com/OpenDroneMap/mvs-texturing/archive/master.zip
+#  GIT_REPOSITORY    https://github.com/OpenDroneMap/mvs-texturing
+#  GIT_TAG           master
+  GIT_REPOSITORY    https://github.com/pierotofy/mvs-texturing
+  GIT_TAG           depth
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
