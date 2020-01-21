1931cc18b3bb2d69f833a6c4357839d3a83379de
Piero Toffanin <pt@masseranolabs.com>
2020-01-21 15:09:17 +0000

    Cleanup old images, repo change

--- SuperBuild/cmake/External-MvsTexturing.cmake
+++ SuperBuild/cmake/External-MvsTexturing.cmake
@@ -8,10 +8,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-#  GIT_REPOSITORY    https://github.com/OpenDroneMap/mvs-texturing
-#  GIT_TAG           master
-  GIT_REPOSITORY    https://github.com/pierotofy/mvs-texturing
-  GIT_TAG           depth
+  GIT_REPOSITORY    https://github.com/OpenDroneMap/mvs-texturing
+  GIT_TAG           master
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
