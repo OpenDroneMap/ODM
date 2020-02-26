de9c09c854a1288bd6eab1f5b734fd8749f5382d
Piero Toffanin <pt@masseranolabs.com>
2020-02-26 21:06:39 +0000

    EXIF/XMP parsing and refactoring
    Former-commit-id: 850500ac0d00efb1be4624dd89d93315ac535897

---
+++ opendm/multispectral.py
@@ -0,0 +1,9 @@
+def dn_to_radiance(photo, img):
+    """
+    Convert Digital Number values to Radiance values
+    :param photo ODM_Photo
+    :param img numpy array containing image data
+    :return numpy array with radiance image values
+    """
+    # TODO!
+    return img
