791f936dfaf213112401f4e8d9ead4a104b4210e
NChamo <nicolas@chamo.com.ar>
2020-03-11 09:17:37 -0300

    Avoid using delaney when bounds are actually a box
    Former-commit-id: ae1e36d8291c759e1981e08f0f71315d122fdd5d

--- opendm/dem/ground_rectification/bounds/types.py
+++ opendm/dem/ground_rectification/bounds/types.py
@@ -36,12 +36,37 @@ class PolyBounds(object):
     def corners(self):
         return self._corners
 
-    def points(self):
-        return self.__points
-
-class BoxBounds(PolyBounds):
+class BoxBounds(object):
     def __init__(self, x_min, x_max, y_min, y_max):
-        super(BoxBounds, self).__init__([[x_min, y_max], [x_max, y_max], [x_max, y_min], [x_min, y_min]])
+        self._corners = (x_min, x_max, y_min, y_max)
+
+    def keep_points_inside(self, point_cloud):
+        """Return a new point cloud with the points from the given cloud that are inside the bounds"""
+        mask = self.calculate_mask(point_cloud)
+        return point_cloud[mask]
+
+    def percentage_of_points_inside(self, points):
+        if isinstance(points, PointCloud):
+            points = points.get_xy()
+        mask = self.calculate_mask(points)
+        return np.count_nonzero(mask) * 100 / points.shape[0]
+
+    def calculate_mask(self, points):
+        """Calculate the mask that would filter out the points outside the bounds"""
+        if isinstance(points, PointCloud):
+            points = points.get_xy()
+        (x_min, x_max, y_min, y_max) = self._corners
+        min = np.array([x_min, y_min])
+        max = np.array([x_max, y_max])
+
+        return np.all(np.logical_and(min <= points, points <= max), axis=1)
+
+    def center(self):
+        (x_min, x_max, y_min, y_max) = self._corners
+        return ((x_min + x_max) / 2, (y_min + y_max) / 2)
+
+    def corners(self):
+        return self._corners
 
     def area(self):
         (x_min, x_max, y_min, y_max) = self._corners
