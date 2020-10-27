756092149e4a7c92d42c23f1f9a45dd4fde4abc9
Piero Toffanin <pt@masseranolabs.com>
2020-10-27 17:21:28 -0400

    Add External-OpenMVS.cmake

---
+++ SuperBuild/cmake/External-OpenMVS.cmake
@@ -0,0 +1,42 @@
+set(_proj_name openmvs)
+set(_SB_BINARY_DIR "${SB_BINARY_DIR}/${_proj_name}")
+
+externalproject_add(vcg
+    GIT_REPOSITORY  https://github.com/cdcseacave/VCG.git
+    GIT_TAG         master
+    UPDATE_COMMAND  ""
+    SOURCE_DIR      ${SB_SOURCE_DIR}/vcg
+    CONFIGURE_COMMAND ""
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND   ""
+    INSTALL_COMMAND ""
+)
+
+ExternalProject_Add(${_proj_name}
+  DEPENDS           ceres opencv vcg
+  PREFIX            ${_SB_BINARY_DIR}
+  TMP_DIR           ${_SB_BINARY_DIR}/tmp
+  STAMP_DIR         ${_SB_BINARY_DIR}/stamp
+  #--Download step--------------
+  DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
+  GIT_REPOSITORY    https://github.com/cdcseacave/openMVS
+  GIT_TAG           0af021346007f1ab15af160814977a91a8166f55
+  #--Update/Patch step----------
+  UPDATE_COMMAND    ""
+  #--Configure step-------------
+  SOURCE_DIR        ${SB_SOURCE_DIR}/${_proj_name}
+  CMAKE_ARGS
+    -DCERES_ROOT_DIR=${SB_INSTALL_DIR}
+    -DOpenCV_DIR=${SB_INSTALL_DIR}/lib/cmake/opencv4
+    -DVCG_ROOT=${SB_SOURCE_DIR}/vcg
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX=${SB_INSTALL_DIR}
+  #--Build step-----------------
+  BINARY_DIR        ${_SB_BINARY_DIR}
+  #--Install step---------------
+  INSTALL_DIR       ${SB_INSTALL_DIR}
+  #--Output logging-------------
+  LOG_DOWNLOAD      OFF
+  LOG_CONFIGURE     OFF
+  LOG_BUILD         OFF
+)
