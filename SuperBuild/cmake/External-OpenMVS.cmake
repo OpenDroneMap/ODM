4af4e4007b6982de6acb2c580b05be2743c821de
Piero Toffanin <pt@masseranolabs.com>
2020-11-02 15:58:25 -0500

    Get OpenMVS from ODM repo

--- SuperBuild/cmake/External-OpenMVS.cmake
+++ SuperBuild/cmake/External-OpenMVS.cmake
@@ -19,8 +19,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/cdcseacave/openMVS
-  GIT_TAG           v1.1.1
+  GIT_REPOSITORY    https://github.com/OpenDroneMap/openMVS
+  GIT_TAG           210
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
