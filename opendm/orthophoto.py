add3cd5af802dd8cf525dd0ebc264c7810bc0e4a
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2021-04-15 11:31:55 -0400

    remove osparam from systemrun

--- opendm/orthophoto.py
+++ opendm/orthophoto.py
@@ -66,7 +66,7 @@ def generate_kmz(orthophoto_file, output_file=None, outsize=None):
         bandparam = "-b 1 -b 2 -b 3 -a_nodata 0"
 
     system.run('gdal_translate -of KMLSUPEROVERLAY -co FORMAT=JPEG "%s" "%s" %s %s '
-               '--config GDAL_CACHEMAX %s%% ' % (orthophoto_file, output_file, osparam, bandparam, get_max_memory()))    
+               '--config GDAL_CACHEMAX %s%% ' % (orthophoto_file, output_file, bandparam, get_max_memory()))    
     
 def post_orthophoto_steps(args, bounds_file_path, orthophoto_file, orthophoto_tiles_dir):
     if args.crop > 0:
