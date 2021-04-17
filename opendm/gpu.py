64861119cc10e22911d6cc7a3c73c6112456c333
Piero Toffanin <pt@masseranolabs.com>
2021-04-17 11:28:35 -0400

    Update gpu.py

--- opendm/gpu.py
+++ opendm/gpu.py
@@ -11,7 +11,7 @@ def has_gpus():
     try:
         import pyopencl
     except:
-        log.ODM_INFO("PyOpenCL is missing (not a GPU build)")
+        log.ODM_INFO("NOt using the GPU")
         return False
 
     try:
