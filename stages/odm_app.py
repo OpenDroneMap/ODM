579a4399e30dca84a1233bc65d85e5bb897d3b1b
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-10-06 08:55:24 -0600

    update flying best practices link
    link was broken

--- stages/odm_app.py
+++ stages/odm_app.py
@@ -108,7 +108,7 @@ class ODMApp:
 
             if code == 139 or code == 134 or code == 1:
                 # Segfault
-                log.ODM_ERROR("Uh oh! Processing stopped because of strange values in the reconstruction. This is often a sign that the input data has some issues or the software cannot deal with it. Have you followed best practices for data acquisition? See https://docs.opendronemap.org/flying.html")
+                log.ODM_ERROR("Uh oh! Processing stopped because of strange values in the reconstruction. This is often a sign that the input data has some issues or the software cannot deal with it. Have you followed best practices for data acquisition? See https://docs.opendronemap.org/flying/")
             elif code == 137:
                 log.ODM_ERROR("Whoops! You ran out of memory! Add more RAM to your computer, if you're using docker configure it to use more memory, for WSL2 make use of .wslconfig (https://docs.microsoft.com/en-us/windows/wsl/wsl-config#configure-global-options-with-wslconfig), resize your images, lower the quality settings or process the images using a cloud provider (e.g. https://webodm.net).")
             elif code == 132:
