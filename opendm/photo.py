851030ff49b02e642ffa6df68ff12df65f5ce7f8
Piero Toffanin <pt@masseranolabs.com>
2022-01-18 16:39:09 +0000

    Sensefly roll flip

--- opendm/photo.py
+++ opendm/photo.py
@@ -175,12 +175,14 @@ class ODM_Photo:
                 if 'Image Make' in tags:
                     try:
                         self.camera_make = tags['Image Make'].values
+                        self.camera_make = self.camera_make.strip()
                     except UnicodeDecodeError:
                         log.ODM_WARNING("EXIF Image Make might be corrupted")
                         self.camera_make = "unknown"
                 if 'Image Model' in tags:
                     try:
                         self.camera_model = tags['Image Model'].values
+                        self.camera_model = self.camera_model.strip()
                     except UnicodeDecodeError:
                         log.ODM_WARNING("EXIF Image Model might be corrupted")
                         self.camera_model = "unknown"
@@ -352,6 +354,9 @@ class ODM_Photo:
                     if self.has_ypr():
                         if self.camera_make.lower() in ['dji', 'hasselblad']:
                             self.pitch = 90 + self.pitch
+                    
+                        if self.camera_make.lower() == 'sensefly':
+                            self.roll *= -1
 
                 except Exception as e:
                     log.ODM_WARNING("Cannot read XMP tags for %s: %s" % (_path_file, str(e)))
