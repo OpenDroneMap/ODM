b1c9152cc826d08c689d9ef73efc69fae22c0027
Piero Toffanin <pt@masseranolabs.com>
2022-06-16 13:28:42 -0400

    Change rs_correct call

--- opendm/osfm.py
+++ opendm/osfm.py
@@ -72,7 +72,7 @@ class OSFMContext:
             rs_file = self.path('rs_done.txt')
 
             if not io.file_exists(rs_file) or rerun:
-                self.run('rs_correct --output reconstruction.json --output-tracks tracks.csv')
+                self.run('rs_correct')
 
                 log.ODM_INFO("Re-running the reconstruction pipeline")
 
