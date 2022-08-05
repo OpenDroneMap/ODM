0bbd16d24fa4a1367ff540b22bce24016872a910
Piero Toffanin <pt@masseranolabs.com>
2022-08-05 11:18:22 -0400

    Remove var

--- stages/splitmerge.py
+++ stages/splitmerge.py
@@ -31,16 +31,11 @@ class ODMSplitStage(types.ODM_Stage):
         should_split = len(photos) > args.split
 
         if should_split:
-            gps_info_available = False
-            
             # check for availability of either image_groups.txt (split-merge) or geotagged photos
             image_groups_file = os.path.join(args.project_path, "image_groups.txt")
             if 'split_image_groups_is_set' in args:
                 image_groups_file = os.path.abspath(args.split_image_groups)
             if io.file_exists(image_groups_file) or reconstruction.has_geotagged_photos():
-                gps_info_available = True
-
-            if gps_info_available:
                 outputs['large'] = True
             else:
                 log.ODM_WARNING('Could not perform split-merge as GPS information in photos or image_groups.txt is missing.')
