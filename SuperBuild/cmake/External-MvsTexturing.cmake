3637cadbf3820a4eeb57af895d34886b7ade7370
Piero Toffanin <pt@masseranolabs.com>
2022-09-01 09:39:16 +0200

    Builds mvstexturing

--- SuperBuild/cmake/External-MvsTexturing.cmake
+++ SuperBuild/cmake/External-MvsTexturing.cmake
@@ -19,6 +19,7 @@ ExternalProject_Add(${_proj_name}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
     ${WIN32_CMAKE_ARGS}
+    ${APPLE_CMAKE_ARGS}
   #--Build step-----------------
   BINARY_DIR        ${_SB_BINARY_DIR}
   #--Install step---------------
