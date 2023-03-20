61cff70be6392431cb0f7627e6617eb50b78124d
Piero Toffanin <pt@masseranolabs.com>
2023-03-20 13:40:24 -0400

    Add Eigen, Win32 args

--- SuperBuild/cmake/External-OpenPointClass.cmake
+++ SuperBuild/cmake/External-OpenPointClass.cmake
@@ -2,7 +2,7 @@ set(_proj_name openpointclass)
 set(_SB_BINARY_DIR "${SB_BINARY_DIR}/${_proj_name}")
 
 ExternalProject_Add(${_proj_name}
-  DEPENDS           pdal
+  DEPENDS           pdal eigen34
   PREFIX            ${_SB_BINARY_DIR}
   TMP_DIR           ${_SB_BINARY_DIR}/tmp
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
@@ -17,8 +17,10 @@ ExternalProject_Add(${_proj_name}
   CMAKE_ARGS
     -DPDAL_DIR=${SB_INSTALL_DIR}/lib/cmake/PDAL
     -DWITH_GBT=ON
+    -DEIGEN3_INCLUDE_DIR=${SB_SOURCE_DIR}/eigen34/
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
+    ${WIN32_CMAKE_ARGS}
   #--Build step-----------------
   BINARY_DIR        ${_SB_BINARY_DIR}
   #--Install step---------------
