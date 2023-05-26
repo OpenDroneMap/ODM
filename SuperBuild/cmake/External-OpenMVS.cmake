12b8f4391271b0650a7c435b0accb976495f3f02
Piero Toffanin <pt@masseranolabs.com>
2023-05-26 14:03:09 -0400

    Update OpenMVS

--- SuperBuild/cmake/External-OpenMVS.cmake
+++ SuperBuild/cmake/External-OpenMVS.cmake
@@ -53,7 +53,7 @@ ExternalProject_Add(${_proj_name}
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
   GIT_REPOSITORY    https://github.com/OpenDroneMap/openMVS
-  GIT_TAG           301
+  GIT_TAG           317
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
