dd5e451448007bed4ae284c34ce08da93790cdba
Sebastien <bonaime@ipgp.fr>
2023-06-07 16:31:35 +0200

    Update run_opensfm.py
    typo radiometric-calibration and  radiometric_calibration

--- stages/run_opensfm.py
+++ stages/run_opensfm.py
@@ -116,7 +116,7 @@ class ODMOpenSfMStage(types.ODM_Stage):
             if photo.is_thermal():
                 return thermal.dn_to_temperature(photo, image, tree.dataset_raw)
             else:
-                return multispectral.dn_to_reflectance(photo, image, use_sun_sensor=args.radiometric_calibration=="camera+sun")
+                return multispectral.dn_to_reflectance(photo, image, use_sun_sensor=args.radiometric-calibration=="camera+sun")
 
 
         def align_to_primary_band(shot_id, image):
