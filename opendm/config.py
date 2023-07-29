08e54077ea3a3b79e5391c41b6e69881512aca71
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2023-07-29 01:03:57 -0400

    when vs. if

--- opendm/config.py
+++ opendm/config.py
@@ -273,7 +273,7 @@ def config(argv=None, parser=None):
                         nargs=0,
                         default=False,
                         help='Ignore Ground Sampling Distance (GSD).'
-                        'A memory and processor hungry change to the default behavior when set to true. '
+                        'A memory and processor hungry change relative to the default behavior if set to true. '
                         'Ordinarily, GSD estimates are used to cap the maximum resolution of image outputs and resizes images when necessary, resulting in faster processing and lower memory usage. '
                         'Since GSD is an estimate, sometimes ignoring it can result in slightly better image output quality. '
                         'Never set --ignore-gsd to true unless you are positive you need it, and even then: don't use it. Default: %(default)s')
