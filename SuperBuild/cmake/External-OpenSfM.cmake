48b7ed4297783205a255d62a5796d65a9a2a23e3
Piero Toffanin <pt@masseranolabs.com>
2023-11-19 11:27:33 -0500

    Feat: track decimation

--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -24,8 +24,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/OpenSfM/
-  GIT_TAG           330
+  GIT_REPOSITORY    https://github.com/pierotofy/OpenSfM/
+  GIT_TAG           trackdec
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
   #--Configure step-------------
