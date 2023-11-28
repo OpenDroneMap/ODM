b08f955963c446efb3f52e326d6515d876e6f620
Piero Toffanin <pt@masseranolabs.com>
2023-11-28 11:33:09 -0500

    Use URL

--- SuperBuild/cmake/External-PDAL.cmake
+++ SuperBuild/cmake/External-PDAL.cmake
@@ -16,8 +16,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/PDAL
-  GIT_TAG           333
+  URL               https://github.com/OpenDroneMap/PDAL/archive/refs/heads/333.zip
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
