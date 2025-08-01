3ee1f122af8ea06765669a01faf6153dd6dc4095
Nathan Olson <nathanmolson@gmail.com>
2025-07-31 17:53:22 -0700

    compiling!

--- SuperBuild/cmake/External-PDAL.cmake
+++ SuperBuild/cmake/External-PDAL.cmake
@@ -16,7 +16,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  URL               https://github.com/PDAL/PDAL/archive/refs/tags/2.9.0.zip
+  GIT_REPOSITORY    https://github.com/NathanMOlson/PDAL/
+  GIT_TAG           0082ff73521085a8d43e6fdf5060797fc216445a
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
