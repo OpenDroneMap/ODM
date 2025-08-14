877fb00e6ce30636a19426240c356682c9a5280e
Nathan <nathanmolson@gmail.com>
2025-08-14 10:14:52 -0700

    change unit test to match current behavior, which sets the "--skip-report" option

--- tests/test_osfm.py
+++ tests/test_osfm.py
@@ -19,40 +19,40 @@ class TestOSFM(unittest.TestCase):
         # Base + project name
         args = config.config(["--project-path", "/datasets", "brighton"])
         self.assertEqual(get_submodel_argv(args)[1:], 
-            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel'])
+            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report'])
         self.assertEqual(get_submodel_argv(args, "/submodels", "submodel_0000")[1:], 
-            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--project-path', '/submodels', 'submodel_0000'])        
+            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report', '--project-path', '/submodels', 'submodel_0000'])        
 
         # Project name + base
         args = config.config(["brighton", "--project-path", "/datasets"])
         self.assertEqual(get_submodel_argv(args)[1:], 
-            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel'])
+            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report'])
         self.assertEqual(get_submodel_argv(args, "/submodels", "submodel_0000")[1:], 
-            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--project-path', '/submodels', 'submodel_0000'])        
+            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report', '--project-path', '/submodels', 'submodel_0000'])        
 
         # Crop
         args = config.config(["brighton", "--project-path", "/datasets", "--crop", "0"])
         self.assertEqual(get_submodel_argv(args)[1:], 
-            ['--crop', '0.015625', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel'])
+            ['--crop', '0.015625', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report'])
         self.assertEqual(get_submodel_argv(args, "/submodels", "submodel_0000")[1:], 
-            ['--crop', '0.015625', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--project-path', '/submodels', 'submodel_0000'])        
+            ['--crop', '0.015625', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report', '--project-path', '/submodels', 'submodel_0000'])        
 
         # With sm-cluster, pc-csv and others
         args = config.config(["--project-path", "/datasets", "--split", "200", "--pc-csv"])
         self.assertEqual(get_submodel_argv(args)[1:], 
-            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel'])
+            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report'])
         self.assertEqual(get_submodel_argv(args, "/submodels", "submodel_0000")[1:], 
-            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--project-path', '/submodels', 'submodel_0000'])        
+            ['--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report', '--project-path', '/submodels', 'submodel_0000'])        
 
         # Cameras JSON
         args = config.config(["--project-path", "/datasets", "--cameras", os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets", "sample.json")])
         self.assertEqual(get_submodel_argv(args)[1:], 
-            ['--cameras', '{"test": "1"}', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel'])
+            ['--cameras', '{"test": "1"}', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report'])
         
         # Camera JSON string
         args = config.config(["--project-path", "/datasets", "--cameras", '{"test": "1"}'])
         self.assertEqual(get_submodel_argv(args)[1:], 
-            ['--cameras', '{"test": "1"}', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel'])
+            ['--cameras', '{"test": "1"}', '--orthophoto-cutline', '--dem-euclidean-map', '--skip-3dmodel', '--skip-report'])
     
     def test_get_submodel_argv_dict(self):
         # Base
