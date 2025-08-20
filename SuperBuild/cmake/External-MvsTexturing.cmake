cee164c777463c4645ba6548e8e065b045006473
Nathan <nathanmolson@gmail.com>
2025-08-20 14:02:30 -0700

    attempt to build MvsTexturing with upstream-style cmake.

--- SuperBuild/cmake/External-MvsTexturing.cmake
+++ SuperBuild/cmake/External-MvsTexturing.cmake
@@ -2,14 +2,14 @@ set(_proj_name mvstexturing)
 set(_SB_BINARY_DIR "${SB_BINARY_DIR}/${_proj_name}")
 
 ExternalProject_Add(${_proj_name}
-  DEPENDS           mve
+  DEPENDS
   PREFIX            ${_SB_BINARY_DIR}
   TMP_DIR           ${_SB_BINARY_DIR}/tmp
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/mvs-texturing
-  GIT_TAG           c5a4d0c9a434553533c6e39d426e349fcfa5f48d
+  GIT_REPOSITORY    https://github.com/NathanMOlson/mvs-texturing
+  GIT_TAG           e0229675f48d303e451fec61a5405cd8fea67c85
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
