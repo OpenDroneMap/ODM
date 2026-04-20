e04016d6bbb29dd6153c19094a9fdb28c75443a7
Charles Milette <charles.milette@gmail.com>
2026-04-19 21:45:30 -0400

    Update to 2.7.0

--- configure.py
+++ configure.py
@@ -28,7 +28,7 @@ parser.add_argument('--build-vcpkg',
                     help='Build VCPKG environment from scratch instead of downloading prebuilt one.')
 parser.add_argument('--vcpkg-archive-url',
                     type=str,
-                    default='https://github.com/sylveon/windows-deps/releases/download/test/vcpkg-export.zip',
+                    default='https://github.com/OpenDroneMap/windows-deps/releases/download/2.7.0/vcpkg-export.zip',
                     required=False,
                     help='Path to VCPKG export archive')
 parser.add_argument('--signtool-path',
@@ -204,7 +204,7 @@ def dist():
     # Download portable python
     if not os.path.isdir("python312"):
         pythonzip_path = os.path.join("SuperBuild", "download", "python312.zip")
-        python_url = "https://github.com/OpenDroneMap/windows-deps/releases/download/2.6.1/python-3.12.9-embed-amd64-less-pth.zip"
+        python_url = "https://github.com/OpenDroneMap/windows-deps/releases/download/2.7.0/python-3.12.9-embed-amd64-less-pth-sqlite.zip"
         if not os.path.exists(pythonzip_path):
             print("Downloading %s" % python_url)
             with urllib.request.urlopen(python_url) as response, open( pythonzip_path, 'wb') as out_file:
