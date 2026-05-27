c7dc3a39e2b93725c89e68cc96359f4b4f5ec09a
Michael Johnson <mjohnson459@gmail.com>
2026-05-27 13:18:33 +0100

    Move laszip and untwine to conda deps

--- SuperBuild/cmake/Conda-Packages.cmake
+++ SuperBuild/cmake/Conda-Packages.cmake
@@ -1,7 +1,7 @@
 # Conda/pixi packages required at configure time (see pixi.toml).
 
-function(odm_require_conda_package name version)
-  find_package(${name} ${version} EXACT REQUIRED)
+function(odm_require_conda_package name)
+  find_package(${name} REQUIRED)
   if(${name}_FOUND AND ${name}_VERSION)
     message(STATUS "${name} ${${name}_VERSION} found (conda)")
   else()
